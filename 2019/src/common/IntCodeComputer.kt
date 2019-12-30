6807e45a194d39d7b2c19342cbdaf5e0e1c14f89
Alex Williams <alex.williams@skybettingandgaming.com>
2019-12-30 03:28:20 +0000

    Day 11, plus interesting kotlin compiler bug

---
+++ 2019/src/common/IntCodeComputer.kt
@@ -0,0 +1,156 @@
+package common
+
+
+@Suppress("ArrayInDataClass")
+data class RunState(
+    val memory: LongArray,
+    val inputs: List<Long> = emptyList(),
+    val outputs: List<Long> = emptyList(),
+    val ip: Int = 0,
+    val state: MachineState = MachineState.RUNNING,
+    val base: Int = 0,
+    val sparseMem: Map<Int, Long> = emptyMap()
+)
+
+tailrec fun runProgram(state: RunState): RunState {
+    val (memory, inputs, outputs, ip) = state
+    val (opcode, modes) = decodeInstruction(memory[ip])
+    return when (opcode) {
+        Opcode.ADD -> runProgram(
+            state.updateMemory(
+                state.paramAddress(3, modes[2]),
+                state.paramValue(1, modes[0])
+                        + state.paramValue(2, modes[1])
+            ).copy(ip = ip + 4)
+        )
+        Opcode.MULTIPLY -> runProgram(
+            state.updateMemory(
+                state.paramAddress(3, modes[2]),
+                state.paramValue(1, modes[0])
+                        * state.paramValue(2, modes[1])
+            ).copy(ip = ip + 4)
+        )
+        Opcode.INPUT ->
+            if (inputs.isEmpty())
+                state.copy(state = MachineState.YIELDING_ON_MISSING_INPUT)
+            else runProgram(
+                state.updateMemory(state.paramAddress(1, modes[0]), inputs.first())
+                    .copy(
+                        inputs = inputs.drop(1),
+                        ip = ip + 2,
+                        state = MachineState.RUNNING
+                    )
+            )
+        Opcode.OUTPUT -> {
+            runProgram(
+                state.copy(
+                    outputs = outputs.plus(state.paramValue(1, modes[0])),
+                    ip = ip + 2
+                )
+            )
+        }
+        Opcode.JUMP_IF_TRUE -> runProgram(
+            state.copy(
+                ip = if (state.paramValue(1, modes[0]) != 0L)
+                    state.paramValue(2, modes[1]).toInt()
+                else ip + 3
+            )
+        )
+        Opcode.JUMP_IF_FALSE -> runProgram(
+            state.copy(
+                ip = if (state.paramValue(1, modes[0]) == 0L)
+                    state.paramValue(2, modes[1]).toInt()
+                else ip + 3
+            )
+        )
+        Opcode.LESS_THAN -> runProgram(
+            state.updateMemory(
+                state.paramAddress(3, modes[2]),
+                if (state.paramValue(1, modes[0])
+                    < state.paramValue(2, modes[1])
+                ) 1 else 0
+            ).copy(ip = ip + 4)
+        )
+        Opcode.EQUALS -> runProgram(
+            state.updateMemory(
+                state.paramAddress(3, modes[2]),
+                if (state.paramValue(1, modes[0])
+                    == state.paramValue(2, modes[1])
+                ) 1 else 0
+            ).copy(ip = ip + 4)
+        )
+        Opcode.BASE_OFFSET -> runProgram(
+            state.copy(
+                base = state.base + state.paramValue(1, modes[0]).toInt(),
+                ip = ip + 2
+            )
+        )
+        Opcode.HALT -> state.copy(state = MachineState.HALTED, ip = ip + 1)
+    }
+}
+
+
+private fun RunState.updateMemory(address: Int, value: Long) =
+    if (address < this.memory.size)
+        this.copy(memory = memory.withValue(address, value))
+    else
+        this.copy(sparseMem = sparseMem.plus(address to value))
+
+private fun LongArray.withValue(address: Int, value: Long) = this.clone().apply { this[address] = value }
+
+private fun RunState.getMemory(address: Int) =
+    if (address < this.memory.size)
+        this.memory[address]
+    else
+        this.sparseMem[address] ?: 0
+
+private fun RunState.paramValue(param: Int, parameterMode: ParameterMode): Long =
+    when (parameterMode) {
+        ParameterMode.IMMEDIATE -> getMemory(ip + param)
+        ParameterMode.POSITION -> getMemory(getMemory(ip + param).toInt())
+        ParameterMode.RELATIVE -> getMemory(getMemory(ip + param).toInt() + base)
+    }
+
+private fun RunState.paramAddress(param: Int, parameterMode: ParameterMode): Int =
+    when (parameterMode) {
+        ParameterMode.IMMEDIATE -> error("Cannot find address of immediate parameter")
+        ParameterMode.POSITION -> getMemory(ip + param).toInt()
+        ParameterMode.RELATIVE -> getMemory(ip + param).toInt() + base
+    }
+
+private fun decodeInstruction(input: Long): Pair<Opcode, List<ParameterMode>> {
+    val inputAsString = input.toString().padStart(5, '0')
+    return Pair(
+        opcodeMap[inputAsString.takeLast(2).toInt()] ?: error("Unknown opcode $input"),
+        inputAsString.reversed().drop(2).map { modeMap[it - '0'] ?: error("Unknown mode: $input") }
+    )
+}
+
+private enum class ParameterMode(val encoding: Int) {
+    POSITION(0),
+    IMMEDIATE(1),
+    RELATIVE(2)
+}
+
+private val modeMap = ParameterMode.values().associateBy { it.encoding }
+
+private enum class Opcode(val encoding: Int) {
+    ADD(1),
+    MULTIPLY(2),
+    INPUT(3),
+    OUTPUT(4),
+    JUMP_IF_TRUE(5),
+    JUMP_IF_FALSE(6),
+    LESS_THAN(7),
+    EQUALS(8),
+    BASE_OFFSET(9),
+    HALT(99)
+}
+
+private val opcodeMap = Opcode.values().associateBy { it.encoding }
+
+enum class MachineState {
+    RUNNING,
+    YIELDING_ON_MISSING_INPUT,
+    HALTED
+}
---
+++ 2019/src/day11/Main.kt
@@ -0,0 +1,136 @@
+package day11
+
+import common.MachineState
+import common.RunState
+import common.fromClasspathFileToLongProgram
+import common.runProgram
+import kotlin.test.assertEquals
+
+
+fun main() {
+    runTests()
+
+    val memory = "day11/input.txt".fromClasspathFileToLongProgram()
+
+    val picture = paint(memory, startingPanel = Colour.BLACK)
+    println("Part 1: size = ${picture.size}")
+    assertEquals(1894, picture.size)
+
+    val betterPicture = paint(memory, startingPanel = Colour.WHITE)
+    println("Part 2:")
+    println(betterPicture.renderToString())
+    assertEquals(
+        """
+        ░░░██░█░░█░████░█░░░░████░░░██░███░░█░░█░░░
+        ░░░░█░█░█░░░░░█░█░░░░░░░█░░░░█░█░░█░█░░█░░░
+        ░░░░█░██░░░░░█░░█░░░░░░█░░░░░█░███░░████░░░
+        ░░░░█░█░█░░░█░░░█░░░░░█░░░░░░█░█░░█░█░░█░░░
+        ░█░░█░█░█░░█░░░░█░░░░█░░░░█░░█░█░░█░█░░█░░░
+        ░░██░░█░░█░████░████░████░░██░░███░░█░░█░░░
+    """.trimIndent(), betterPicture.renderToString()
+    )
+}
+
+private fun paint(memory: LongArray, startingPanel: Colour): Map<XY, Long> {
+    return paint(
+        RobotState(
+            intComputer = RunState(memory),
+            position = XY(0, 0),
+            heading = Heading.NORTH,
+            visitedNodes = mapOf(XY(0, 0) to startingPanel.value)
+        )
+    )
+}
+
+private tailrec fun paint(state: RobotState): Map<XY, Long> =
+    with(advanceRobot(state, ::runProgram)) {
+        return@paint if (this.intComputer.state == MachineState.HALTED) this.visitedNodes else paint(this)
+    }
+
+private fun advanceRobot(state: RobotState, programRunner: (RunState) -> RunState = ::runProgram): RobotState {
+    val initialColour = state.visitedNodes[state.position] ?: 0
+
+    val newState = programRunner(state.intComputer.copy(inputs = state.intComputer.inputs.plus(initialColour)))
+    val (colourToPaint, rotation) = newState.outputs.takeLast(2)
+
+    val newVisitedNodes = state.visitedNodes.plus(state.position to colourToPaint.toColour().value)
+    val newHeading = if (rotation == 0L) state.heading.rotateAntiClockwise() else state.heading.rotateClockwise()
+    val newPosition = state.position.afterMoving(newHeading)
+
+    return RobotState(
+        heading = newHeading,
+        position = newPosition,
+        visitedNodes = newVisitedNodes,
+        intComputer = newState
+    )
+}
+
+private fun Map<XY, Long>.renderToString(): String {
+    val xs = map { it.key.x }.min()!!..map { it.key.x }.max()!!
+    val ys = map { it.key.y }.max()!! downTo map { it.key.y }.min()!!
+    return ys.joinToString("\n") { y ->
+        xs.joinToString("") { x ->
+            if (this.getOrDefault(XY(x, y), 0L) == 1L) "█" else "░"
+        }
+    }
+}
+
+private data class XY(val x: Int, val y: Int) {
+    override fun toString() = "[$x,$y]"
+}
+
+private enum class Heading { NORTH, EAST, SOUTH, WEST }
+private enum class Colour(val value: Long) { BLACK(0), WHITE(1) }
+
+private fun Long.toColour() = when (this) {
+    0L -> Colour.BLACK
+    1L -> Colour.WHITE
+    else -> error("Unknown colour value: $this")
+}
+
+private fun Heading.rotateAntiClockwise() = when (this) {
+    Heading.NORTH -> Heading.WEST
+    Heading.EAST -> Heading.NORTH
+    Heading.SOUTH -> Heading.EAST
+    Heading.WEST -> Heading.SOUTH
+}
+
+private fun Heading.rotateClockwise() = when (this) {
+    Heading.NORTH -> Heading.EAST
+    Heading.EAST -> Heading.SOUTH
+    Heading.SOUTH -> Heading.WEST
+    Heading.WEST -> Heading.NORTH
+}
+
+private fun XY.afterMoving(heading: Heading) = when (heading) {
+    Heading.NORTH -> this.copy(y = y + 1)
+    Heading.EAST -> this.copy(x = x + 1)
+    Heading.SOUTH -> this.copy(y = y - 1)
+    Heading.WEST -> this.copy(x = x - 1)
+}
+
+private data class RobotState(
+    val heading: Heading,
+    val position: XY,
+    val visitedNodes: Map<XY, Long>,
+    val intComputer: RunState
+)
+
+
+private fun runTests() {
+    val mockComputer = RunState(longArrayOf())
+    val initialState = RobotState(
+        intComputer = mockComputer,
+        heading = Heading.NORTH,
+        position = XY(0, 0),
+        visitedNodes = emptyMap()
+    )
+    val after = advanceRobot(initialState) {
+        assertEquals(1, it.inputs.size) // there should be one input
+        assertEquals(0, it.inputs.single()) // it should represent a black panel
+        it.copy(outputs = it.outputs + listOf(1L, 0L))
+    }
+    assertEquals(Heading.WEST, after.heading)
+    assertEquals(XY(-1, 0), after.position)
+    assertEquals(1L, after.visitedNodes[XY(0, 0)])
+}
