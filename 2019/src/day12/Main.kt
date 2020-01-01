030ededb66e81983a08f8109074fec053f2f0c75
Alex Williams <alex.williams@skybettingandgaming.com>
2020-01-01 16:20:26 +0000

    day 12 part 2

--- 2019/src/day12/Main.kt
+++ 2019/src/day12/Main.kt
@@ -7,6 +7,20 @@ import kotlin.streams.toList
 import kotlin.test.assertEquals
 
 
+private data class XYZ(val x: Int, val y: Int, val z: Int) {
+    fun applyVector(vector: XYZ) = XYZ(x + vector.x, y + vector.y, z + vector.z)
+    fun sum() = x.absoluteValue + y.absoluteValue + z.absoluteValue
+}
+
+private data class PV(val pos: Int, val vel: Int)
+
+private data class MoonState(val position: XYZ, val velocity: XYZ = XYZ(0, 0, 0)) {
+    val totalEnergy get() = position.sum() * velocity.sum()
+}
+
+private val List<MoonState>.totalEnergy get() = this.sumBy { it.totalEnergy }
+
+
 fun main() {
     runTests()
 
@@ -21,41 +35,68 @@ fun main() {
 
     val afterStep1000 = moonStartPositions.stepSequence().drop(1000).first()
     println("Part 1: total energy = ${afterStep1000.totalEnergy}")
+    assertEquals(12490, afterStep1000.totalEnergy)
+
+
+    val (ix, iy, iz) = findRepeatedStates(moonStartPositions)
+    val lcmSteps = lcm(ix, iy, iz)
+    println("Part 2: [X=$ix; Y=$iy; Z=$iz]; LCM of Steps=$lcmSteps")
+    assertEquals(392733896255168L, lcmSteps)
 }
 
-private data class XYZ(val x: Int, val y: Int, val z: Int) {
-    override fun toString() = "[$x,$y,$z]"
-    fun applyVector(vector: XYZ) = XYZ(x + vector.x, y + vector.y, z + vector.z)
-    fun sum() = x.absoluteValue + y.absoluteValue + z.absoluteValue
+
+private tailrec fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
+private fun lcm(a: Long, b: Long) = a / gcd(a, b) * b
+private fun lcm(vararg xs: Long) = xs.fold(1L) { acc, l -> lcm(acc, l) }
+
+
+private fun findRepeatedStates(initialMoons: List<MoonState>): Triple<Long, Long, Long> {
+    // Doing this with an immutable set caused so much allocation and GC that the process didn't return in a reasonable time.
+    tailrec fun Iterator<List<PV>>.findRepeat(acc: MutableSet<List<PV>> = mutableSetOf()): Long =
+        if (acc.add(this.next()))
+            this.findRepeat(acc)
+        else
+            acc.size.toLong()
+
+    val x = initialMoons.map { PV(it.position.x, it.velocity.x) }.stepIterator().findRepeat()
+    val y = initialMoons.map { PV(it.position.y, it.velocity.y) }.stepIterator().findRepeat()
+    val z = initialMoons.map { PV(it.position.z, it.velocity.z) }.stepIterator().findRepeat()
+    return Triple(x, y, z)
 }
 
-private data class MoonState(val position: XYZ, val velocity: XYZ = XYZ(0, 0, 0)) {
-    val totalEnergy get() = position.sum() * velocity.sum()
-}
 
-private fun List<MoonState>.takeStep(): List<MoonState> {
-    return map { moon ->
-        val newVelocity = fold(moon.velocity) { velocity, otherMoon ->
-            XYZ(
-                x = velocity.x + pretendGravity(moon.position.x, otherMoon.position.x),
-                y = velocity.y + pretendGravity(moon.position.y, otherMoon.position.y),
-                z = velocity.z + pretendGravity(moon.position.z, otherMoon.position.z)
-            )
-        }
-        MoonState(position = moon.position.applyVector(newVelocity), velocity = newVelocity)
+private fun List<PV>.takePVStep(): List<PV> = map { moonAxis ->
+    val newVelocity = fold(moonAxis.vel) { velocity, otherMoon ->
+        velocity + pretendGravity(moonAxis.pos, otherMoon.pos)
     }
+    PV(moonAxis.pos + newVelocity, newVelocity)
 }
 
-private val List<MoonState>.totalEnergy get() = this.sumBy { it.totalEnergy }
 
-private fun List<MoonState>.stepSequence() =
-    generateSequence(this) { it.takeStep() }
+private fun List<MoonState>.takeStep(): List<MoonState> = map { moon ->
+    val newVelocity = fold(moon.velocity) { velocity, otherMoon ->
+        XYZ(
+            x = velocity.x + pretendGravity(moon.position.x, otherMoon.position.x),
+            y = velocity.y + pretendGravity(moon.position.y, otherMoon.position.y),
+            z = velocity.z + pretendGravity(moon.position.z, otherMoon.position.z)
+        )
+    }
+    MoonState(position = moon.position.applyVector(newVelocity), velocity = newVelocity)
+}
 
 private fun pretendGravity(thisMoon: Int, otherMoon: Int) = when {
     otherMoon > thisMoon -> 1
     otherMoon < thisMoon -> -1
     else -> 0
 }
+
+
+private fun List<MoonState>.stepSequence() =
+    generateSequence(this) { it.takeStep() }
+
+private fun List<PV>.stepIterator() =
+    generateSequence(this) { it.takePVStep() }.iterator()
+
 
 private fun runTests() {
     val initialMoons = listOf(
@@ -100,4 +141,13 @@ private fun runTests() {
     assertEquals(MoonState(XYZ(16, -13, 23), XYZ(7, 1, 1)), after100[3])
 
     assertEquals(1940, after100.totalEnergy)
+
+    val (ix, iy, iz) = findRepeatedStates(initialMoons)
+    val lcmSteps = lcm(ix, iy, iz)
+
+    val afterLcmSteps = initialMoons.stepSequence().drop(lcmSteps.toInt()).first()
+    assertEquals(2772, lcmSteps)
+    assertEquals(initialMoons, afterLcmSteps)
+
 }
+
