ee1ef2e2042eb5c3deb00e03ee2b8481cbddc4a0
Alex Williams <alex.williams1@starlingbank.com>
2022-12-07 13:42:27 +0000

    2022 Day 7

--- 2022/src/common/Common.kt
+++ 2022/src/common/Common.kt
@@ -8,6 +8,12 @@ fun String.fromClasspathFileToLines(): List<String> {
     return File(url.toURI()).readLines()
 }
 
+fun String.fromClasspathFile(): String {
+    val url = Common::class.java.classLoader.getResource(this)
+        ?: throw Exception("Could not find file '$this'")
+    return File(url.toURI()).readText()
+}
+
 infix fun IntRange.fullyContains(other: IntRange) = this.contains(other.first) && this.contains(other.last)
 infix fun IntRange.overlaps(other: IntRange) = this.intersect(other).isNotEmpty()
 
@@ -20,5 +26,12 @@ fun <T> List<List<T>>.transpose(): List<List<T>> =
 
 fun <T : CharSequence> List<T>.filterNotBlank() = this.filter { it.isNotBlank() }
 fun <T : CharSequence> List<T>.mapMatching(regex: Regex) = this.mapNotNull { regex.matchEntire(it)?.destructured }
+
+tailrec fun <T> List<T>.startsWith(other: List<T>): Boolean {
+    if (other.isEmpty()) return true
+    if (this.isEmpty()) return false
+    if (this.first() != other.first()) return false
+    return this.drop(1).startsWith(other.drop(1))
+}
 
 object Common
