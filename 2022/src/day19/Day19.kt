1f909167f325fc5eda5c41d3b141e384be5f6e81
Alex Williams <alex.williams1@starlingbank.com>
2022-12-21 03:38:52 +0000

    2022 Day 19

--- 2022/src/day19/Day19.kt
+++ 2022/src/day19/Day19.kt
@@ -1,7 +1,6 @@
 package day19
 
 import common.*
-import day19.InputParser.parseInput
 import day19.RobotType.*
 import kotlinx.coroutines.*
 import kotlin.math.*
@@ -10,14 +9,14 @@ import kotlin.test.*
 private val exampleInput = "day19/example.txt".fromClasspathFileToLines()
 private val puzzleInput = "day19/input.txt".fromClasspathFileToLines()
 private const val PART_1_EXPECTED_EXAMPLE_ANSWER = 33
-private const val PART_2_EXPECTED_EXAMPLE_ANSWER = 62
+private const val PART_2_EXPECTED_EXAMPLE_ANSWER = 62 * 56
 
 fun main() {
-//    assertEquals(PART_1_EXPECTED_EXAMPLE_ANSWER, part1(exampleInput))
-//    part1(puzzleInput).also { println("Part 1: $it") } // 960
-//
+    assertEquals(PART_1_EXPECTED_EXAMPLE_ANSWER, part1(exampleInput))
+    part1(puzzleInput).also { println("Part 1: $it") } // 960, took 33.8s
+
     assertEquals(PART_2_EXPECTED_EXAMPLE_ANSWER, part2(exampleInput))
-    part2(puzzleInput).also { println("Part 2: $it") } //
+    part2(puzzleInput).also { println("Part 2: $it") } // 2040, took 4m 43.4s ... good enough.
 }
 
 private fun part1(input: List<String>): Int {
@@ -44,24 +43,15 @@ private fun part2(input: List<String>): Int {
 }
 
 
-private fun maxGeodesFromRobotRecipe(bestSoFar: IntArray, robotCreationOrder: List<RobotType>, blueprint: InputParser.Blueprint, minutes: Int): Int {
+private fun maxGeodesFromRobotRecipe(bestSoFar: IntArray, robotCreationOrder: List<RobotType>, blueprint: Blueprint, minutes: Int): Int {
     if (robotCreationOrder.size > minutes) return 0 // this is not a valid recipe
-    if (willNeverBeValidRobotRecipe(robotCreationOrder)) {
-//        println("Rejecting clearly invalid recipe $robotCreationOrder")
+    if (willNeverStartValidRobotRecipe(robotCreationOrder)) {
         return 0
     }
 
     val geodesFromThisRecipe = simulate(bestSoFar, robotCreationOrder, minutes, blueprint)
-    if (geodesFromThisRecipe == -1) {
-//        println("Recipe did not terminate in time $robotCreationOrder")
-        return 0
-    } // this recipe and all that start with this sequence of robots will always run out of time
-    if (geodesFromThisRecipe == -2) {
-//        println("Recipe starting order $robotCreationOrder could never beat current best")
-        return 0
-    } // this recipe and all that start with this sequence of robots will always run out of time
-//    if (geodesFromThisRecipe > 0)
-//        println("Recipe $robotCreationOrder produced $geodesFromThisRecipe")
+    if (geodesFromThisRecipe == -1) return 0 // this recipe and all that start with this sequence of robots will always run out of time
+    if (geodesFromThisRecipe == -2) return 0 // this sequence prefix could never produce a better result than already found elsewhere
 
     return max(
         geodesFromThisRecipe,
@@ -72,7 +62,7 @@ private fun maxGeodesFromRobotRecipe(bestSoFar: IntArray, robotCreationOrder: Li
     )
 }
 
-private fun simulate(bestSoFar: IntArray, robotCreationOrder: List<RobotType>, deadline: Int, bp: InputParser.Blueprint): Int {
+private fun simulate(bestSoFar: IntArray, robotCreationOrder: List<RobotType>, deadline: Int, bp: Blueprint): Int {
     var oreRobots = 1
     var clayRobots = 0
     var obsidianRobots = 0
@@ -83,11 +73,6 @@ private fun simulate(bestSoFar: IntArray, robotCreationOrder: List<RobotType>, d
     var geodes = 0
     var time = 0
 
-//    if (!isValidRobotRecipe(robotCreationOrder)) {
-//        println("Not a valid recipe: $robotCreationOrder")
-//        return 0
-//    }
-
     for (nextRobot in robotCreationOrder) {
         val turnsToAcquireResources = when (nextRobot) {
             ORE -> max(0, (bp.oreOreCost - ore) divideRoundingUp oreRobots)
@@ -95,7 +80,6 @@ private fun simulate(bestSoFar: IntArray, robotCreationOrder: List<RobotType>, d
             OBSIDIAN -> max(0, (bp.obsidianOreCost - ore) divideRoundingUp oreRobots, (bp.obsidianClayCost - clay) divideRoundingUp clayRobots)
             GEODE -> max(0, (bp.geodeOreCost - ore) divideRoundingUp oreRobots, (bp.geodeObsidianCost - obsidian) divideRoundingUp obsidianRobots)
         }
-//        println("Needs $turnsToAcquireResources turns for resource gathering for $nextRobot")
 
         val turnsToBuildRobot = turnsToAcquireResources + 1
         time += turnsToBuildRobot
@@ -129,17 +113,13 @@ private fun simulate(bestSoFar: IntArray, robotCreationOrder: List<RobotType>, d
 //        println("Created $nextRobot at time $time; $ore+=$oreRobots ore, $clay+=$clayRobots clay, $obsidian+=$obsidianRobots obs, $geodes+=$geodeRobots")
     }
     val totalGeodesProduced = geodes + (geodeRobots * (deadline - time))
+    if (bestSoFar[0] < totalGeodesProduced) {
+        bestSoFar[0] = totalGeodesProduced
+    }
     return totalGeodesProduced
-        .also {
-            if (bestSoFar[0] < it) {
-                bestSoFar[0] = it
-                println("Found new best: $it, $robotCreationOrder")
-            }
-        }
-//        .also { println("Had $deadline-$time=${deadline - time} minutes to increase to $it geodes") }
 }
 
-private fun willNeverBeValidRobotRecipe(listOfMachines: List<RobotType>): Boolean {
+private fun willNeverStartValidRobotRecipe(listOfMachines: List<RobotType>): Boolean {
     val firstClay = listOfMachines.indexOfFirst { it == CLAY }
     val firstObsidian = listOfMachines.indexOfFirst { it == OBSIDIAN }
     val firstGeode = listOfMachines.indexOfFirst { it == GEODE }
@@ -148,46 +128,30 @@ private fun willNeverBeValidRobotRecipe(listOfMachines: List<RobotType>): Boolea
     return false
 }
 
-private fun isValidRobotRecipe(listOfMachines: List<RobotType>): Boolean {
-    val firstOre = 0
-    val firstClay = listOfMachines.indexOfFirst { it == CLAY }
-    if (firstClay < firstOre) return false
-    val firstObsidian = listOfMachines.indexOfFirst { it == OBSIDIAN }
-    if (firstObsidian < firstClay) return false
-    val firstGeode = listOfMachines.indexOfFirst { it == GEODE }
-    if (firstGeode < firstObsidian) return false
-    return true
-}
-
-
 private enum class RobotType { ORE, CLAY, OBSIDIAN, GEODE }
 
+private val inputMatcher =
+    Regex("Blueprint (\\d+): Each ore robot costs (\\d+) ore. Each clay robot costs (\\d+) ore." + " Each obsidian robot costs (\\d+) ore and (\\d+) clay. Each geode robot costs (\\d+) ore and (\\d+) obsidian.")
 
-private object InputParser {
-    private val inputMatcher = Regex(
-        "Blueprint (\\d+): Each ore robot costs (\\d+) ore. Each clay robot costs (\\d+) ore." + " Each obsidian robot costs (\\d+) ore and (\\d+) clay. Each geode robot costs (\\d+) ore and (\\d+) obsidian."
-    )
+private fun parseInput(input: List<String>): List<Blueprint> =
+    input.mapMatching(inputMatcher).map { (number, oreCost, clayCost, obsidianOreCost, obsidianClayCost, geodeOreCost, geodeObsidianCost) ->
+        Blueprint(
+            number.toInt(),
+            oreCost.toInt(),
+            clayCost.toInt(),
+            obsidianOreCost.toInt(),
+            obsidianClayCost.toInt(),
+            geodeOreCost.toInt(),
+            geodeObsidianCost.toInt()
+        )
+    }
 
-    fun parseInput(input: List<String>): List<Blueprint> =
-        input.mapMatching(inputMatcher).map { (number, oreCost, clayCost, obsidianOreCost, obsidianClayCost, geodeOreCost, geodeObsidianCost) ->
-            Blueprint(
-                number.toInt(),
-                oreCost.toInt(),
-                clayCost.toInt(),
-                obsidianOreCost.toInt(),
-                obsidianClayCost.toInt(),
-                geodeOreCost.toInt(),
-                geodeObsidianCost.toInt()
-            )
-        }
-
-    data class Blueprint(
-        val number: Int,
-        val oreOreCost: Int,
-        val clayOreCost: Int,
-        val obsidianOreCost: Int,
-        val obsidianClayCost: Int,
-        val geodeOreCost: Int,
-        val geodeObsidianCost: Int
-    )
-}
+private data class Blueprint(
+    val number: Int,
+    val oreOreCost: Int,
+    val clayOreCost: Int,
+    val obsidianOreCost: Int,
+    val obsidianClayCost: Int,
+    val geodeOreCost: Int,
+    val geodeObsidianCost: Int
+)
