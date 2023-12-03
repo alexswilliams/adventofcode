83bccf97332b5408a1acf73eb8c83a8876d82110
Alex Williams <lexw@live.co.uk>
2023-12-03 13:59:24 +0000

    optimise day 3

--- 2023/aoc2023/day3/Day3.kt
+++ 2023/aoc2023/day3/Day3.kt
@@ -14,21 +14,24 @@ fun main() {
     part2(exampleInput).also { println("[Example] Part 2: $it") }.also { assertEquals(467835, it) }
     part2(puzzleInput).also { println("[Puzzle] Part 2: $it") }.also { assertEquals(79842967, it) }
 
-    benchmark { part1(puzzleInput) } // 547µs
-    benchmark { part2(puzzleInput) } // 615µs
+    benchmark { part1(puzzleInput) } // 530µs
+    benchmark { part2(puzzleInput) } // 542µs
 }
 
-sealed interface ItemAtCoordinate
-data class SymbolAtCoordinate(val symbol: Char, val row: Int, val col: Int) : ItemAtCoordinate
-data class IntAtCoordinate(val value: Int, val row: Int, val colFirst: Int, val colLast: Int) : ItemAtCoordinate
+private sealed interface ItemAtCoordinate
+private data class SymbolAtCoordinate(val symbol: Char, val row: Int, val col: Int) : ItemAtCoordinate
+private data class IntAtCoordinate(val value: Int, val row: Int, val colFirst: Int, val colLast: Int) :
+    ItemAtCoordinate {
+    fun isAdjacentTo(symbol: SymbolAtCoordinate): Boolean =
+        symbol.row in (row - 1..row + 1) && symbol.col in (colFirst - 1..colLast + 1)
+}
 
 private fun part1(input: List<CharArray>): Int {
     val (symbols, numbers) = tokeniseInput(input)
     return numbers
         .filter { number ->
             symbols.any {
-                it.row in (number.row - 1..number.row + 1) &&
-                        it.col in (number.colFirst - 1..number.colLast + 1)
+                number.isAdjacentTo(it)
             }
         }
         .sumOf { it.value }
@@ -38,16 +41,18 @@ private fun part2(input: List<CharArray>): Int {
     val (symbols, numbers) = tokeniseInput(input)
     return symbols
         .filter { it.symbol == '*' }
-        .mapNotNull { gear ->
+        .sumOf { gear ->
             val touchingNumbers = numbers.filter {
-                gear.row in (it.row - 1..it.row + 1) && gear.col in (it.colFirst - 1..it.colLast + 1)
-            }.map { it.value }
-            if (touchingNumbers.size == 2) touchingNumbers[0] * touchingNumbers[1] else null
-        }.sum()
+                it.isAdjacentTo(gear)
+            }
+            if (touchingNumbers.size == 2)
+                touchingNumbers[0].value * touchingNumbers[1].value
+            else 0
+        }
 }
 
 private fun tokeniseInput(input: List<CharArray>): Pair<List<SymbolAtCoordinate>, List<IntAtCoordinate>> {
-    val result = arrayListOf<SymbolAtCoordinate>() to arrayListOf<IntAtCoordinate>()
+    val result = ArrayList<SymbolAtCoordinate>(1000) to ArrayList<IntAtCoordinate>(1500)
     input.forEachIndexed { row, line ->
         var value = 0
         var startsAt = -1
@@ -62,9 +67,7 @@ private fun tokeniseInput(input: List<CharArray>): Pair<List<SymbolAtCoordinate>
                     value = 0
                     startsAt = -1
                 }
-                if (elem != '.') {
-                    result.first.add(SymbolAtCoordinate(elem, row, index))
-                }
+                if (elem != '.') result.first.add(SymbolAtCoordinate(elem, row, index))
             }
             index++
         }
