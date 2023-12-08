3f8a38abf0f7c52f614f3b1cec1e0a4448408fd3
Alex Williams <lexw@live.co.uk>
2023-12-08 04:03:43 +0000

    Day 7 speed improvement

--- 2023/aoc2023/day7/Day7.kt
+++ 2023/aoc2023/day7/Day7.kt
@@ -2,7 +2,6 @@ package aoc2023.day7
 
 import common.benchmark
 import common.fromClasspathFileToLines
-import common.frequency
 import kotlin.test.assertEquals
 
 
@@ -19,75 +18,87 @@ fun main() {
 }
 
 
-private data class Hand(val cardOrderStrength: Int, val type: PokerType, val bid: Int)
+private data class Hand(val ordering: Int, val bid: Int)
 private enum class PokerType(val strength: Int) {
-    FIVE_O_A_K(7),
-    FOUR_O_A_K(6),
+    FIVE_OF_A_KIND(7),
+    FOUR_OF_A_KIND(6),
     FULL_HOUSE(5),
-    THREE_O_A_K(4),
+    THREE_OF_A_KIND(4),
     TWO_PAIR(3),
     ONE_PAIR(2),
     HIGH_CARD(1)
 }
 
 private fun part1(input: List<String>): Int {
-    return input.map { it.split(' ') }
-        .map { (cards, bid) -> Hand(cardOrderingStrength(cards, part1Ordering), classifyCards(cards), bid.toInt()) }
-        .sortedWith(compareBy<Hand> { hand -> hand.type.strength }.thenComparing { hand -> hand.cardOrderStrength })
+    return input.map { it.split(' ', limit = 2) }
+        .map { (cards, bid) -> Hand(classifyPokerHand(cards) * fieldShift + cardOrderingStrength(cards, part1Ordering), bid.toInt()) }
+        .sortedBy { hand -> hand.ordering }
         .withIndex()
         .sumOf { (index, hand) -> (index + 1) * hand.bid }
 }
 
 private fun part2(input: List<String>): Int {
-    return input.map { it.split(' ') }
-        .map { (cards, bid) -> Hand(cardOrderingStrength(cards, part2Ordering), classifyCardsWithJoker(cards), bid.toInt()) }
-        .sortedWith(compareBy<Hand> { hand -> hand.type.strength }.thenComparing { hand -> hand.cardOrderStrength })
+    return input.map { it.split(' ', limit = 2) }
+        .map { (cards, bid) -> Hand(classifyCardsWithJoker(cards) * fieldShift + cardOrderingStrength(cards, part2Ordering), bid.toInt()) }
+        .sortedBy { hand -> hand.ordering }
         .withIndex()
         .sumOf { (index, hand) -> (index + 1) * hand.bid }
 }
 
-private fun classifyCardsWithJoker(cards: String): PokerType {
-    if ('J' !in cards) return classifyCards(cards)
+private fun classifyCardsWithJoker(cards: String): Int {
+    if ('J' !in cards) return classifyPokerHand(cards)
     val nonJokers = cards.filterNot { it == 'J' }
     val jokerCount = 5 - nonJokers.length
-    if (jokerCount >= 4) return PokerType.FIVE_O_A_K
+    if (jokerCount >= 4) return PokerType.FIVE_OF_A_KIND.strength
     return when (jokerCount) {
-        1 -> listOf(nonJokers + nonJokers[0], nonJokers + nonJokers[1], nonJokers + nonJokers[2], nonJokers + nonJokers[3])
-        2 -> listOf(
+        1 -> sequenceOf(nonJokers + nonJokers[0], nonJokers + nonJokers[1], nonJokers + nonJokers[2], nonJokers + nonJokers[3])
+        2 -> sequenceOf(
             nonJokers + nonJokers[0] + nonJokers[0], nonJokers + nonJokers[0] + nonJokers[1], nonJokers + nonJokers[0] + nonJokers[2],
             nonJokers + nonJokers[1] + nonJokers[1], nonJokers + nonJokers[1] + nonJokers[2],
             nonJokers + nonJokers[2] + nonJokers[2],
-        )
+        ).distinct()
 
-        else -> listOf(
+        else -> sequenceOf(
             nonJokers + nonJokers[0] + nonJokers[0] + nonJokers[0],
             nonJokers + nonJokers[0] + nonJokers[0] + nonJokers[1],
             nonJokers + nonJokers[0] + nonJokers[1] + nonJokers[1],
             nonJokers + nonJokers[1] + nonJokers[1] + nonJokers[1],
-        )
-    }.map { classifyCards(it) }.maxBy { it.strength }
+        ).distinct()
+    }.map { classifyPokerHand(it) }.max()
 }
 
-private fun classifyCards(cards: String): PokerType {
-    val groups = cards.frequency()
-    return when {
-        groups.size == 1 -> PokerType.FIVE_O_A_K
-        groups.size == 2 && groups.any { it.second == 4 } -> PokerType.FOUR_O_A_K
-        groups.size == 2 && groups.any { it.second == 3 } -> PokerType.FULL_HOUSE
-        groups.size == 3 && groups.any { it.second == 3 } -> PokerType.THREE_O_A_K
-        groups.size == 3 && groups.any { it.second == 1 } && groups.any { it.second == 2 } -> PokerType.TWO_PAIR
-        groups.size == 4 -> PokerType.ONE_PAIR
-        groups.size == 5 -> PokerType.HIGH_CARD
-        else -> throw Exception("Unexpected card type for $cards -> $groups")
+private fun classifyPokerHand(cards: String): Int {
+    val counts = intArrayOf(1, 1, 1, 1, 1)
+    var unique = 5
+    for (i in 1..4) {
+        for (j in 0..<i) {
+            if (cards[i] == cards[j]) {
+                counts[j]++
+                counts[i] = 0
+                unique--
+                break
+            }
+        }
     }
+    return when {
+        unique == 1 -> PokerType.FIVE_OF_A_KIND
+        unique == 4 -> PokerType.ONE_PAIR
+        unique == 5 -> PokerType.HIGH_CARD
+        unique == 2 && counts.any { it == 4 } -> PokerType.FOUR_OF_A_KIND
+        unique == 2 && counts.any { it == 3 } -> PokerType.FULL_HOUSE
+        unique == 3 && counts.any { it == 3 } -> PokerType.THREE_OF_A_KIND
+        unique == 3 && counts.any { it == 1 } && counts.any { it == 2 } -> PokerType.TWO_PAIR
+        else -> throw Exception("Unexpected card type for $cards -> $counts")
+    }.strength
 }
 
 private const val part1Ordering = "23456789TJQKA"
 private const val part2Ordering = "J23456789TQKA"
+private const val fieldShift = 13 * 13 * 13 * 13 * 13
 private fun cardOrderingStrength(cards: String, ordering: String): Int {
-    return ordering.indexOf(cards[0]) * 100_00_00_00 +
-            ordering.indexOf(cards[1]) * 100_00_00 +
-            ordering.indexOf(cards[2]) * 100_00 +
-            ordering.indexOf(cards[3]) * 100 +
+    return ordering.indexOf(cards[0]) * 13 * 13 * 13 * 13 +
+            ordering.indexOf(cards[1]) * 13 * 13 * 13 +
+            ordering.indexOf(cards[2]) * 13 * 13 +
+            ordering.indexOf(cards[3]) * 13 +
             ordering.indexOf(cards[4])
 }
