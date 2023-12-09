a0019531a679a5182525c4cc62999a1fca166d36
Alex Williams <lexw@live.co.uk>
2023-12-09 15:58:50 +0000

    Sequences were faster

--- 2023/aoc2023/day9/Day9.kt
+++ 2023/aoc2023/day9/Day9.kt
@@ -12,19 +12,18 @@ fun main() {
     part1(puzzleInput).also { println("[Puzzle] Part 1: $it") }.also { assertEquals(1666172641, it) }
     part2(exampleInput).also { println("[Example] Part 2: $it") }.also { assertEquals(2, it) }
     part2(puzzleInput).also { println("[Puzzle] Part 2: $it") }.also { assertEquals(933, it) }
-    benchmark { part1(puzzleInput) } // 801µs
-    benchmark { part2(puzzleInput) } // 548µs
+    benchmark { part1(puzzleInput) } // 557µs
+    benchmark { part2(puzzleInput) } // 381µs
 }
 
-private fun part1(input: List<String>) = sumOfNextValues(input) { ints, step -> ints.last() + step }
-private fun part2(input: List<String>) = sumOfNextValues(input) { ints, step -> ints.first() - step }
+private fun part1(input: List<String>) = sumOfNextValues(input) { (_, last), step -> last + step }
+private fun part2(input: List<String>) = sumOfNextValues(input) { (first, _), step -> first - step }
 
-private fun sumOfNextValues(input: List<String>, findStepAbove: (List<Int>, Int) -> Int): Int =
+private fun sumOfNextValues(input: List<String>, findStepAbove: (Pair<Int, Int>, Int) -> Int): Int =
     input.map { it.splitToInts(" ") }
-        .sumOf { startLine ->
-            val differences = mutableListOf(startLine)
-            while (differences.last().any { it != 0 }) {
-                differences.add(differences.last().zipWithNext { a, b -> b - a })
-            }
-            differences.foldRight(0, findStepAbove)
+        .sumOf { initial ->
+            generateSequence(initial) { line -> if (line.all { it == 0 }) null else line.zipWithNext { a, b -> b - a } }
+                .map { it.first() to it.last() }
+                .toList()
+                .foldRight(0, findStepAbove)
         }
