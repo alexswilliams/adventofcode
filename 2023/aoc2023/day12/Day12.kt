faa51d8d4825955a65dc918e8a7c12b1df5e5304
Alex Williams <alex.williams1@starlingbank.com>
2023-12-13 09:26:08 +0000

    out of heap space :(

--- 2023/aoc2023/day12/Day12.kt
+++ 2023/aoc2023/day12/Day12.kt
@@ -1,5 +1,6 @@
 package aoc2023.day12
 
+import common.benchmark
 import common.fromClasspathFileToLines
 import common.splitToInts
 import kotlinx.coroutines.Dispatchers
@@ -8,6 +9,7 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import java.time.Instant
 import kotlin.test.assertEquals
+import kotlin.time.measureTimedValue
 
 
 private val exampleInput = "aoc2023/day12/example.txt".fromClasspathFileToLines()
@@ -16,12 +18,12 @@ private val puzzleInput = "aoc2023/day12/input.txt".fromClasspathFileToLines()
 fun main() {
     part1(exampleInput).also { println("[Example] Part 1: $it") }.also { assertEquals(21, it) }
     part1(puzzleInput).also { println("[Puzzle] Part 1: $it") }.also { assertEquals(7032, it) }
+    benchmark { part1(puzzleInput) } // 715µs
     part2(exampleInput).also { println("[Example] Part 2: $it") }.also { assertEquals(525152, it) }
-//    val x = measureTimedValue { part2(puzzleInput).also { println("[Puzzle] Part 2: $it") } } // TODO: dynamic programming
-//    println(x.duration)
+    benchmark(10) { part2(exampleInput) } // 9ms
+    val x = measureTimedValue { part2(puzzleInput).also { println("[Puzzle] Part 2: $it") } } // TODO: dynamic programming
+    println(x.duration)
 //    part2(puzzleInput).also { println("[Puzzle] Part 2: $it") }.also { assertEquals(0, it) }
-//    benchmark { part1(puzzleInput) } // 2.46ms
-//    benchmark(10) { part2(exampleInput) } // 60ms
 }
 
 private fun part1(input: List<String>): Int {
@@ -30,12 +32,12 @@ private fun part1(input: List<String>): Int {
         val runLengths = runLengthString.splitToInts(",")
         val template = ".$templateInput."
 
-        numberOfArrangements(runLengths, template)
+        numberOfArrangements(runLengths, template.toCharArray())
     }
 }
 
 
-private fun part2(input: List<String>): Int {
+private fun part2(input: List<String>): Long {
     return runBlocking(Dispatchers.Default) {
         input.mapIndexed { index, line ->
             val (templateInput, runLengthString) = line.split(' ', limit = 2)
@@ -45,31 +47,45 @@ private fun part2(input: List<String>): Int {
 
             async {
                 println("[${Instant.now()}]  Starting Row $index")
-                numberOfArrangements(runLengths, template)
+                numberOfArrangements(runLengths, template.toCharArray()).toLong()
                     .also { println("[${Instant.now()}]  Finished Row $index -> size $it") }
             }
         }.awaitAll().sum()
     }
 }
 
-private fun numberOfArrangements(runLengths: List<Int>, template: String) =
-    runLengths.fold(listOf(".")) { prefixes, runLength ->
-        prefixes.flatMap { placementsForRun(template, runLength, it, it.length) }
-    }.filter { template.indexOf('#', it.length) == -1 }.size
+private fun numberOfArrangements(runLengths: List<Int>, templateArray: CharArray): Int {
+    return runLengths.fold(listOf(1)) { prefixes, runLength ->
+        val prefixLengths = ArrayList<Int>(1000)
+        prefixes.forEach {
+            prefixLengthsAfterPlacingNextRun(templateArray, runLength, it, prefixLengths)
+        }
+        prefixLengths
+    }.filterNot { templateArray.existsBetween('#', it) }.size
+}
 
-private fun placementsForRun(template: String, runLength: Int, prefix: String, prefixLength: Int): List<String> {
-    var startIndex = prefixLength
-    val prefixesToKeep = arrayListOf<String>()
+private fun prefixLengthsAfterPlacingNextRun(template: CharArray, runLength: Int, prefixLength: Int, prefixesToKeep: ArrayList<Int>) {
+    var startAt = prefixLength
+    val lastIndex = template.lastIndex - runLength
     while (true) {
         while (true) {
-            if (startIndex + runLength > template.lastIndex) return prefixesToKeep
-            if (template[startIndex - 1] == '#') return prefixesToKeep
-            if (template.indexOf('.', startIndex) < (startIndex + runLength) || template[startIndex + runLength] == '#') startIndex++
+            if (startAt > lastIndex) return
+            if (template[startAt - 1] == '#') return
+            if (shouldAdvance(template, startAt, runLength)) startAt++
             else break
         }
-        prefixesToKeep.add(prefix + template.substring(prefixLength, startIndex) + runs[runLength] + ".")
-        startIndex++
+        prefixesToKeep.add(startAt + runLength + 1)
+        startAt++
     }
 }
 
-private val runs = (0..20).map { "#".repeat(it) }.toTypedArray()
+private fun shouldAdvance(template: CharArray, startAt: Int, runLength: Int) =
+    template.existsBetween('.', startAt, startAt + runLength - 1) ||
+            template[startAt + runLength] == '#'
+
+private fun CharArray.existsBetween(c: Char, startAt: Int = 0, endAt: Int = this.lastIndex): Boolean {
+    for (i in startAt..endAt)
+        if (this[i] == c)
+            return true
+    return false
+}
