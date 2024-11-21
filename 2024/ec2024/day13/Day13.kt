9dc7c1fe9aa5ca5618c6112899e35749067b5976
Alex Williams <lexw@live.co.uk>
2024-11-21 03:15:43 +0000

    Day 13 with custom heap

--- 2024/ec2024/day13/Day13.kt
+++ 2024/ec2024/day13/Day13.kt
@@ -1,7 +1,9 @@
 package ec2024.day13
 
-import common.*
-import java.util.*
+import common.ThreePartChallenge
+import common.TreeStack
+import common.benchmark
+import common.fromClasspathFileToLines
 import kotlin.math.absoluteValue
 import kotlin.math.max
 import kotlin.math.min
@@ -18,9 +20,9 @@ internal fun main() {
     Day13.assertPart1Correct()
     Day13.assertPart2Correct()
     Day13.assertPart3Correct()
-    benchmark { part1(puzzleInput) } // 48µs
-    benchmark(100) { part2(puzzle2Input) } // 1.0ms
-    benchmark(100) { part3(puzzle3Input) } // 21.2ms
+    benchmark { part1(puzzleInput) } // 49µs
+    benchmark { part2(puzzle2Input) } // 558µs
+    benchmark(100) { part3(puzzle3Input) } // 5.8ms
 }
 
 internal object Day13 : ThreePartChallenge {
@@ -40,67 +42,61 @@ internal object Day13 : ThreePartChallenge {
     }
 }
 
-private data class Tile(val r: Int, val c: Int, val height: Int, val distToTarget: Int, val pos: Location = r by c) {
-    override fun equals(other: Any?) = (pos == (other as? Tile)?.pos) == true
-    override fun hashCode(): Int = pos.hashCode()
-}
+private data class Tile(val r: Int, val c: Int, val height: Int)
 
-private fun part1(input: List<String>): Int {
-    val allStarts = input.flatMapIndexed { row, it -> it.mapIndexedNotNull { col, ch -> if (ch == 'S') row by col else null } }
-    val end = input.flatMapIndexed { row, it -> it.mapIndexedNotNull { col, ch -> if (ch == 'E') row by col else null } }.single()
-    val grid = input.mapIndexed { row, string ->
-        string.mapIndexed { col, ch ->
-            when (ch) {
-                'S', 'E' -> Tile(row, col, 0, mhd(row by col, end))
-                in '0'..'9' -> Tile(row, col, ch.digitToInt(), mhd(row by col, end))
-                else -> null
+private fun part1(input: List<String>): Int = shortestPathLength(input)
+private fun part2(input: List<String>): Int = shortestPathLength(input)
+private fun part3(input: List<String>): Int = shortestPathLength(input)
+
+private fun shortestPathLength(input: List<String>): Int =
+    aStarSearch(
+        starts = input.flatMapIndexed { row, line -> line.mapIndexedNotNull { col, ch -> if (ch == 'S') Tile(row, col, 0) else null } },
+        end = input.flatMapIndexed { row, line -> line.mapIndexedNotNull { col, ch -> if (ch == 'E') Tile(row, col, 0) else null } }.single(),
+        grid = input.mapIndexed { row, line ->
+            line.mapIndexed { col, ch ->
+                when (ch) {
+                    in '0'..'9', 'S', 'E' -> Tile(row, col, ch.digitToIntOrNull() ?: 0)
+                    else -> null
+                }
             }
         }
+    )
+
+
+private fun aStarSearch(starts: Collection<Tile>, end: Tile, grid: List<List<Tile?>>, heuristic: (Tile) -> Int = { manhattan(it, end) }): Int {
+    val heap = TreeStack<Tile>(heuristic)
+    val shortestPath = Array(grid.size) { row -> IntArray(grid[0].size) { Int.MAX_VALUE } }
+    starts.forEach {
+        shortestPath[it.r][it.c] = 0
+        heap.offer(it, weight = 0)
     }
-    return dijkstra(allStarts, end, grid)
-}
-
-fun mhd(a: Location, b: Location) = (a.row() - b.row()).absoluteValue.toInt() + (a.col() - b.col()).absoluteValue.toInt()
-
-private fun part2(input: List<String>): Int = part1(input)
-private fun part3(input: List<String>): Int = part1(input)
-
-private fun dijkstra(starts: Collection<Location>, end: Location, grid: List<List<Tile?>>): Int {
-    val smallestDistance = Array(grid.size) { row -> IntArray(grid[0].size) { Int.MAX_VALUE } }
-        .apply { starts.forEach { location -> this[location.rowInt()][location.colInt()] = 0 } }
-    val visited = Array(grid.size) { BooleanArray(grid[0].size) }
-        .apply { starts.forEach { location -> this[location.rowInt()][location.colInt()] = true } }
-
-    val priorityQueue = PriorityQueue<Tile> { a, b -> (smallestDistance[a.r][a.c] + a.distToTarget).compareTo(smallestDistance[b.r][b.c] + b.distToTarget) }
-        .apply { starts.forEach { start -> offer(Tile(start.rowInt(), start.colInt(), 0, mhd(start, end))) } }
 
     while (true) {
-        val u = priorityQueue.poll()
-        val distanceSoFar = smallestDistance[u.r][u.c]
-        if (u.pos == end) return distanceSoFar
-        for ((n, distance) in neighboursOf(u, grid)) {
-            if (visited[n.r][n.c]) continue
-            val candidateDistance = distance + distanceSoFar + 1
-            if (smallestDistance[n.r][n.c] > candidateDistance) {
-                smallestDistance[n.r][n.c] = candidateDistance
-                priorityQueue.remove(n)
-                priorityQueue.offer(n)
+        val u = heap.poll() ?: throw Error("All routes explored with no solution")
+        val distanceToU = shortestPath[u.r][u.c]
+        if (u == end) return distanceToU
+
+        for (n in neighboursOf(u, grid)) {
+            val originalDistance = shortestPath[n.r][n.c]
+            val newDistance = distanceBetween(u, n) + distanceToU + 1
+            if (newDistance < originalDistance) {
+                heap.offerOrReposition(n, originalDistance, newDistance)
+                shortestPath[n.r][n.c] = newDistance
             }
         }
-        visited[u.r][u.c] = true
     }
-    throw Error()
 }
 
+private fun manhattan(t1: Tile, t2: Tile) = (t1.r - t2.r).absoluteValue + (t1.c - t2.c).absoluteValue + (t1.height - t2.height).absoluteValue
 
-private fun neighboursOf(tile: Tile, grid: List<List<Tile?>>): Collection<Pair<Tile, Int>> = buildList {
-    if (tile.c > 0) grid[tile.r][tile.c - 1]?.let { add(it to stepsBetween(tile, it)) }
-    if (tile.c < grid[0].lastIndex) grid[tile.r][tile.c + 1]?.let { add(it to stepsBetween(tile, it)) }
-    if (tile.r > 0) grid[tile.r - 1][tile.c]?.let { add(it to stepsBetween(tile, it)) }
-    if (tile.r < grid.lastIndex) grid[tile.r + 1][tile.c]?.let { add(it to stepsBetween(tile, it)) }
+private fun neighboursOf(tile: Tile, grid: List<List<Tile?>>): Collection<Tile> = buildList {
+    if (tile.c > 0) grid[tile.r][tile.c - 1]?.also { add(it) }
+    if (tile.c < grid[0].lastIndex) grid[tile.r][tile.c + 1]?.also { add(it) }
+    if (tile.r > 0) grid[tile.r - 1][tile.c]?.also { add(it) }
+    if (tile.r < grid.lastIndex) grid[tile.r + 1][tile.c]?.also { add(it) }
 }
 
-private fun stepsBetween(a: Tile, b: Tile): Int {
+private fun distanceBetween(a: Tile, b: Tile): Int {
     val bigger = max(a.height, b.height)
     val smaller = min(a.height, b.height)
     return min(bigger - smaller, smaller + 10 - bigger)
---
+++ common/src/main/kotlin/common/TreeStack.kt
@@ -0,0 +1,26 @@
+package common
+
+import java.util.*
+
+class TreeStack<Element>(private val weightOffset: (Element) -> Int = { 0 }) {
+    private val tree = TreeMap<Int, MutableList<Element>>()
+
+    fun offer(e: Element, weight: Int) {
+        tree.getOrPut(weight + weightOffset(e)) { arrayListOf() }.add(e)
+    }
+
+    fun poll(): Element? =
+        tree.firstEntry()?.let { (weight, list) ->
+            if (list.size == 1) {
+                tree.remove(weight)
+                list.single()
+            } else
+                list.removeLast()
+        }
+
+    fun offerOrReposition(e: Element, oldWeight: Int, newWeight: Int) {
+        val weight = oldWeight + weightOffset(e)
+        tree[weight]?.apply { if (remove(e) && isEmpty()) tree.remove(weight) }
+        return offer(e, newWeight)
+    }
+}
