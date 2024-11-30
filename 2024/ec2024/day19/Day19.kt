21b54463566d9127374da8febc0d7f282533b543
Alex Williams <alex.williams1@starlingbank.com>
2024-11-30 14:49:45 +0000

    Day 19

--- 2024/ec2024/day19/Day19.kt
+++ 2024/ec2024/day19/Day19.kt
@@ -7,9 +7,9 @@ private val puzzles = loadFilesToLines("ec2024/day19", "input1.txt", "input2.txt
 
 internal fun main() {
     Day19.assertCorrect()
-    benchmark { part1(puzzles[0]) } // 8.3µs
-    benchmark { part2(puzzles[1]) } // 1.7ms
-//    benchmark(1) { part3(puzzles[2]) }
+    benchmark { part1(puzzles[0]) } // 5.4µs
+    benchmark { part2(puzzles[1]) } // 57µs
+    benchmark { part3(puzzles[2]) } // 870µs
 }
 
 internal object Day19 : Challenge {
@@ -20,69 +20,47 @@ internal object Day19 : Challenge {
         check("VICTORY", "P2 Example") { part2(examples[1]) }
         check("1925785457415943", "P2 Puzzle") { part2(puzzles[1]) }
 
-        error("More coffee needed")
-//        check("", "P3 Puzzle") { part3(puzzles[2]) }
+        check("7625713577112228", "P3 Puzzle") { part3(puzzles[2]) }
     }
 }
 
 
-private fun part1(input: List<String>): String {
+private fun part1(input: List<String>): String = decryptAfterRounds(input, 1)
+private fun part2(input: List<String>): String = decryptAfterRounds(input, 100)
+private fun part3(input: List<String>): String = decryptAfterRounds(input, 1048576000)
+
+
+private fun decryptAfterRounds(input: List<String>, rounds: Int): String {
     val operations = input.first().cyclicIterator()
     val grid = input.drop(2).asArrayOfCharArrays()
-    val rotationPoints = cartesianProductOf(1..grid.lastIndex - 1, 1..grid[0].lastIndex - 1)
 
-    rotationPoints.forEach { center -> grid.rotateSquareAround(center, if (operations.next() == 'L') 1 else -1) }
-
-    val solutionRow = grid.indexOfFirst { '>' in it && '<' in it }
-    return grid[solutionRow].concatToString(grid[solutionRow].indexOf('>') + 1, grid[solutionRow].indexOf('<'))
-}
-
-private fun part2(input: List<String>): String {
-    val operations = input.first().cyclicIterator()
-    val grid = input.drop(2).asArrayOfCharArrays()
-    val rotationPoints = cartesianProductOf(1..grid.lastIndex - 1, 1..grid[0].lastIndex - 1)
-
-    repeat(26 * 3 + 22) { // in the example, the full grid cycles every 26*n+22 iterations; the full example seems not to (in a practical amount of time to brute force)
-        operations.reset()
-        rotationPoints.forEach { center -> grid.rotateSquareAround(center, if (operations.next() == 'L') 1 else -1) }
+    val transforms = mutableListOf<Array<IntArray>>()
+    var lastTransform = buildTransformationMatrix(operations, grid.size, grid[0].size)
+    var target = rounds
+    var lowest = target.takeLowestOneBit()
+    var current = 1
+    while (lowest > 0) {
+        if (lowest == current) {
+            transforms.add(lastTransform)
+            target -= lowest
+            lowest = target.takeLowestOneBit()
+        }
+        lastTransform = lastTransform.applyTransformation(lastTransform)
+        current = current shl 1
     }
 
-    val solutionRow = grid.indexOfFirst { '>' in it && '<' in it }
-    return grid[solutionRow].concatToString(grid[solutionRow].indexOf('>') + 1, grid[solutionRow].indexOf('<'))
+    return findSolutionString(transforms.fold(grid) { lastGrid, transform -> lastGrid.applyTransformation(transform) })
 }
 
-private fun part3(input: List<String>): String {
-    val operations = input.first().cyclicIterator()
-    val grid = input.drop(2).asArrayOfCharArrays()
-    val rotationPoints = cartesianProductOf(1..grid.lastIndex - 1, 1..grid[0].lastIndex - 1).toTypedArray()
-
-    repeat(1048576000) { i ->
-        if (i % 10000 == 0) println((i * 100.0) / 1048576000.0) // could be a loooong time.
-        operations.reset()
-        rotationPoints.forEach { center -> grid.rotateSquareAround(center, if (operations.next() == 'L') 1 else -1) }
-        checkForSolution(grid, i)
+private fun buildTransformationMatrix(operations: Iterator<Char>, rows: Int, cols: Int) =
+    Array(rows) { row -> IntArray(cols) { col -> row by16 col } }.also {
+        val rotationPoints = cartesianProductOf(1..rows - 2, 1..cols - 2).toTypedArray()
+        for (center in rotationPoints) it.rotateSquareAround(center, if (operations.next() == 'L') 1 else -1)
     }
 
-    val solutionRow = grid.indexOfFirst { '>' in it && '<' in it }
-    return grid[solutionRow].concatToString(grid[solutionRow].indexOf('>') + 1, grid[solutionRow].indexOf('<'))
-}
-
-
-private fun checkForSolution(grid: Grid, i: Int) {
-    val solutionRow = grid.indexOfFirst { '>' in it && '<' in it }
-    if (solutionRow != -1 &&
-        grid[solutionRow].indexOf('>') + 1 < grid[solutionRow].indexOf('<') &&
-        grid[solutionRow].concatToString(grid[solutionRow].indexOf('>') + 1, grid[solutionRow].indexOf('<')).all { it.isDigit() }
-    ) {
-        println("Found >...< at iteration $i")
-        grid.forEach { chars -> println(chars.concatToString()) }
-        println()
-    }
-}
-
-private fun Grid.rotateSquareAround(center: Pair<Int, Int>, amountCw: Int) {
+private fun Array<IntArray>.rotateSquareAround(center: Pair<Int, Int>, amountCw: Int) {
     val (row, col) = center
-    val original = charArrayOf(
+    val original = intArrayOf(
         this[row - 1][col - 1], this[row - 1][col], this[row - 1][col + 1],
         this[row][col + 1],
         this[row + 1][col + 1], this[row + 1][col], this[row + 1][col - 1],
@@ -96,4 +74,25 @@ private fun Grid.rotateSquareAround(center: Pair<Int, Int>, amountCw: Int) {
     this[row + 1][col] = original[5 + amountCw]
     this[row + 1][col - 1] = original[6 + amountCw]
     this[row][col - 1] = original[(7 + amountCw) and 7]
+}
+
+private fun Array<IntArray>.applyTransformation(transformation: Array<IntArray>): Array<IntArray> =
+    Array(this.size) { row ->
+        IntArray(this[0].size) { col ->
+            val cell = transformation[row][col]
+            this[cell.row()][cell.col()]
+        }
+    }
+
+private fun Grid.applyTransformation(transformation: Array<IntArray>): Grid =
+    Array(this.size) { row ->
+        CharArray(this[0].size) { col ->
+            val cell = transformation[row][col]
+            this[cell.row()][cell.col()]
+        }
+    }
+
+private fun findSolutionString(grid: Grid): String {
+    val solutionRow = grid.indexOfFirst { '>' in it && '<' in it }
+    return grid[solutionRow].concatToString(grid[solutionRow].indexOf('>') + 1, grid[solutionRow].indexOf('<'))
 }
