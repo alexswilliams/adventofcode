5e03ddd4c942f44eb21930cf082fabcb7eeb8649
Alex Williams <lexw@live.co.uk>
2024-12-01 19:40:34 +0000

    Day 20 Part 3

--- 2024/ec2024/day20/Day20.kt
+++ 2024/ec2024/day20/Day20.kt
@@ -9,7 +9,7 @@ internal fun main() {
     Day20.assertCorrect()
     benchmark { part1(puzzles[0]) } // 468µs
     benchmark(10) { part2(puzzles[1]) } // 255ms
-//    benchmark(100) { part3(puzzles[2]) }
+    benchmark(100) { part3(puzzles[2]) } // 464µs
 }
 
 internal object Day20 : Challenge {
@@ -22,8 +22,8 @@ internal object Day20 : Challenge {
         check(206, "P2 Example C") { part2(examples[3]) }
         check(562, "P2 Puzzle") { part2(puzzles[1]) }
 
-//        check(768790, "P3 Example") { part3(examples[4]) }
-//        check(768791, "P3 Puzzle") { part3(puzzles[2]) } // solved by hand
+        check(768790, "P3 Example") { part3(examples[4]) }
+        check(768791, "P3 Puzzle") { part3(puzzles[2]) }
     }
 }
 
@@ -93,10 +93,64 @@ private fun part2(grid: Grid): Int {
     }
 }
 
-private fun part3(grid: Grid): Int = TODO()
+private fun part3(grid: Grid): Int {
+    fillDeadEnds(grid)
+    val start = grid.location16Of('S')
+
+    // observation - after a short preamble, flying down one column yields the best solutions
+    val lossFromColumns =
+        grid[0].indices.map { col ->
+            col to grid.indices.sumOf { row -> if (grid[row][col] == '#') -grid.size else interpretAirCurrent(grid[row][col]) }
+        }.groupBy({ it.second }) { it.first }
+    val lossFromFutureCycles = lossFromColumns.keys.max()
+    val leastLossyColumns = lossFromColumns[lossFromFutureCycles]!!
+
+    fun maximiseHeightAtBottomOfTargetColumns(start: Location1616, grid: Grid, leastLossyColumns: List<Int>): List<Int> {
+        data class Work(val position: Location1616, val previous: Location1616, val height: Int)
+
+        val work = ArrayDeque(listOf(Work(start, -1, 384_400)))
+        val neighbours = IntArray(4)
+        val bestHeightAt = Array(grid.size) { IntArray(grid[0].size) { Int.MIN_VALUE } }
+        while (true) {
+            val u = work.removeFirstOrNull() ?: return leastLossyColumns.map { bestHeightAt[grid.lastIndex][it] }
+            val bestHeightSoFar = bestHeightAt[u.position.row()][u.position.col()]
+            if (bestHeightSoFar >= u.height) continue
+            bestHeightAt[u.position.row()][u.position.col()] = u.height
+            for (n in descendingNeighboursOfInfiniteGrid(u.position, grid, '#', neighbours)) {
+                if (n == -1 || n == u.previous) continue
+                val newHeight = u.height + interpretAirCurrent(grid[n.row()][n.col()])
+                work.addLast(Work(n, u.position, newHeight))
+            }
+        }
+    }
+
+    return leastLossyColumns
+        .zip(maximiseHeightAtBottomOfTargetColumns(start, grid, leastLossyColumns))
+        .maxOf { (col, heightAfterFirstCycle) ->
+            val cyclesAdvancedOver = (heightAfterFirstCycle / -lossFromFutureCycles) - 1
+            var distanceSouth = (cyclesAdvancedOver + 1) * grid.size - 1
+            var height = heightAfterFirstCycle - cyclesAdvancedOver * -lossFromFutureCycles
+            while (height > 0) {
+                distanceSouth++
+                height = height + interpretAirCurrent(grid[distanceSouth % grid.size][col])
+            }
+            distanceSouth
+        }
+}
+
 
 private fun interpretAirCurrent(op: Char): Int = when (op) {
     '+' -> +1
     '-' -> -2
     else -> -1
+}
+
+fun descendingNeighboursOfInfiniteGrid(pos: Location1616, grid: Grid, wall: Char, output: IntArray = IntArray(4)): IntArray {
+    val colMinus1 = (pos.col() - 1 + grid[0].size) % grid[0].size
+    val colPlus1 = (pos.col() + 1) % grid[0].size
+    val rowPlus1 = (pos.row() + 1) % grid.size
+    output[0] = if (grid[pos.row()][colMinus1] != wall) pos.row() by16 colMinus1 else -1
+    output[2] = if (grid[pos.row()][colPlus1] != wall) pos.row() by16 colPlus1 else -1
+    output[3] = if (grid[rowPlus1][pos.col()] != wall) rowPlus1 by16 pos.col() else -1
+    return output
 }
