39fb4a3411ffefc176e3a4f8ea443be677ae74e8
Alex Williams <alex.williams1@starlingbank.com>
2024-12-08 02:46:24 +0000

    Day 19

--- 2023/aoc2023/day19/Day19.kt
+++ 2023/aoc2023/day19/Day19.kt
@@ -4,22 +4,22 @@ package aoc2023.day19
 
 import common.*
 
-private val examples = loadFilesToLines("aoc2023/day19", "example.txt")
-private val puzzles = loadFilesToLines("aoc2023/day19", "input.txt")
+private val example = loadFilesToLines("aoc2023/day19", "example.txt").single()
+private val puzzle = loadFilesToLines("aoc2023/day19", "input.txt").single()
 
 internal fun main() {
     Day19.assertCorrect()
-    benchmark { part1(puzzles[0]) } // 554µs
-    benchmark { part2(puzzles[0]) }
+    benchmark { part1(puzzle) } // 457µs
+    benchmark { part2(puzzle) } // 145µs
 }
 
 internal object Day19 : Challenge {
     override fun assertCorrect() {
-        check(19114, "P1 Example") { part1(examples[0]) }
-        check(389114, "P1 Puzzle") { part1(puzzles[0]) }
+        check(19114, "P1 Example") { part1(example) }
+        check(389114, "P1 Puzzle") { part1(puzzle) }
 
-        check(0, "P2 Example") { part2(examples[0]) }
-        check(0, "P2 Puzzle") { part2(puzzles[0]) }
+        check(167409079868000L, "P2 Example") { part2(example) }
+        check(125051049836302L, "P2 Puzzle") { part2(puzzle) }
     }
 }
 
@@ -67,4 +67,55 @@ private fun isAccepted(ruleset: Map<String, Rule>, part: Part): Boolean {
     } while (true)
 }
 
-private fun part2(input: List<String>): Long = 0
+private data class Rule2(val name: String, val defaultSink: String, val conditions: List<Condition2>)
+private data class Condition2(val field: Char, val sign: Char, val number: Int, val targetRule: String)
+private data class XmasRange(val x: IntRange, val m: IntRange, val a: IntRange, val s: IntRange)
+
+private fun part2(input: List<String>): Long {
+    val ruleset = input.asSequence()
+        .takeWhile { it.isNotBlank() }
+        .map { rule ->
+            val name = rule.substringBefore('{')
+            val defaultSink = rule.substring(rule.lastIndexOf(',') + 1, rule.length - 1)
+            val conditions = rule.substring(name.length + 1, rule.length - defaultSink.length - 2)
+                .splitMappingRanges(",") { it, start, end -> Condition2(it[start], it[start + 1], it.toIntFromIndex(start + 2), it.substring(it.lastIndexOf(':', end) + 1, end + 1)) }
+            Rule2(name, defaultSink, conditions)
+        }.associateBy { it.name }
+
+    return validRanges(ruleset, "in", 1..4000, 1..4000, 1..4000, 1..4000)
+        .sumOf { 1L * it.x.size * it.m.size * it.a.size * it.s.size }
+}
+
+private fun constrain(i: IntRange, c: Condition2, fieldName: Char): IntRange =
+    if (c.field != fieldName) i
+    else if (c.sign == '>') IntRange(i.first.coerceAtLeast(c.number + 1), i.last)
+    else IntRange(i.first, i.last.coerceAtMost(c.number - 1))
+
+private fun constrainOpposite(i: IntRange, c: Condition2, fieldName: Char): IntRange =
+    if (c.field != fieldName) i
+    else if (c.sign == '<') IntRange(i.first.coerceAtLeast(c.number), i.last)
+    else IntRange(i.first, i.last.coerceAtMost(c.number))
+
+private fun validRanges(ruleset: Map<String, Rule2>, ruleName: String, x: IntRange, m: IntRange, a: IntRange, s: IntRange, soFar: MutableList<XmasRange> = mutableListOf()): List<XmasRange> {
+    if (ruleName == "A") return soFar.apply { add(XmasRange(x, m, a, s)) }
+    if (x.isEmpty() || m.isEmpty() || a.isEmpty() || s.isEmpty()) return soFar
+    val rule = ruleset[ruleName] ?: error("Rule invalid: $ruleName")
+
+    var xRemaining = x
+    var mRemaining = m
+    var aRemaining = a
+    var sRemaining = s
+    rule.conditions.forEach { c ->
+        val xC = constrain(xRemaining, c, 'x')
+        val mC = constrain(mRemaining, c, 'm')
+        val aC = constrain(aRemaining, c, 'a')
+        val sC = constrain(sRemaining, c, 's')
+        xRemaining = constrainOpposite(xRemaining, c, 'x')
+        mRemaining = constrainOpposite(mRemaining, c, 'm')
+        aRemaining = constrainOpposite(aRemaining, c, 'a')
+        sRemaining = constrainOpposite(sRemaining, c, 's')
+        if (c.targetRule != "R") validRanges(ruleset, c.targetRule, xC, mC, aC, sC, soFar)
+    }
+    if (rule.defaultSink != "R") validRanges(ruleset, rule.defaultSink, xRemaining, mRemaining, aRemaining, sRemaining, soFar)
+    return soFar
+}
