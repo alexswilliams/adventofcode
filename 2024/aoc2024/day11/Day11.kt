69bc2f638271b2ef6d39238c4b54f37e2004a638
Alex Williams <alex.williams1@starlingbank.com>
2024-12-11 12:18:29 +0000

    Day 11

--- 2024/aoc2024/day11/Day11.kt
+++ 2024/aoc2024/day11/Day11.kt
@@ -24,30 +24,30 @@ internal object Day11 : Challenge {
 }
 
 
-private fun part1(input: String): Long = with(newCache(25)) { input.splitToLongs(" ").sumOf { countValuesBeneath(25, it, this) } }
-private fun part2(input: String): Long = with(newCache(75)) { input.splitToLongs(" ").sumOf { countValuesBeneath(75, it, this) } }
+private fun part1(input: String): Long = with(newCache(25)) { input.splitToLongs(" ").sumOf { countStonesBeneath(25, it, this) } }
+private fun part2(input: String): Long = with(newCache(75)) { input.splitToLongs(" ").sumOf { countStonesBeneath(75, it, this) } }
 
 
-fun countValuesBeneath(iterRemaining: Int, number: Long, cache: Array<MutableMap<Long, Long>>): Long {
-    if (iterRemaining == 0) return 1
-    cache[iterRemaining - 1][number]?.let { return it }
+fun countStonesBeneath(blinksRemaining: Int, numberOnStone: Long, cache: Array<MutableMap<Long, Long>>): Long {
+    if (blinksRemaining == 0) return 1
+    cache[blinksRemaining - 1][numberOnStone]?.let { return it }
 
-    val digitCount = log10(number.toFloat()).toInt() + 1
-    val countAtThisNode = if (number == 0L)
-        countValuesBeneath(iterRemaining - 1, 1L, cache)
+    val digitCount = log10(numberOnStone.toFloat()).toInt() + 1
+    val countAtThisPoint = if (numberOnStone == 0L)
+        countStonesBeneath(blinksRemaining - 1, 1L, cache)
     else if (digitCount and 1 == 0) {
         val power = powersOfTen[digitCount / 2]
-        val left = number / power
-        val right = number % power
-        countValuesBeneath(iterRemaining - 1, left, cache) + countValuesBeneath(iterRemaining - 1, right, cache)
-    } else countValuesBeneath(iterRemaining - 1, number * 2024, cache)
+        val left = numberOnStone / power
+        val right = numberOnStone % power
+        countStonesBeneath(blinksRemaining - 1, left, cache) + countStonesBeneath(blinksRemaining - 1, right, cache)
+    } else countStonesBeneath(blinksRemaining - 1, numberOnStone * 2024, cache)
 
-    return countAtThisNode.also { cache[iterRemaining - 1][number] = it }
+    return countAtThisPoint.also { cache[blinksRemaining - 1][numberOnStone] = it }
 }
 
-private fun newCache(breathCount: Int): Array<MutableMap<Long, Long>> = Array(breathCount) { i -> newHashMap(cacheSize(breathCount, i)) }
-private fun cacheSize(breathCount: Int, i: Int) =
-    if (breathCount <= 25 || i >= 50) 300
+private fun newCache(blinkCount: Int): Array<MutableMap<Long, Long>> = Array(blinkCount) { i -> newHashMap(cacheSize(blinkCount, i)) }
+private fun cacheSize(blinkCount: Int, i: Int) =
+    if (blinkCount <= 25 || i >= 50) 300
     else if (i >= 40) 1000
     else if (i >= 30) 2000
     else if (i >= 20) 3000
