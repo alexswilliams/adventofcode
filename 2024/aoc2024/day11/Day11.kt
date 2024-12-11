c7bf92937830b6d8b793cfc6b5cb296f5684e7ad
Alex Williams <alex.williams1@starlingbank.com>
2024-12-11 10:51:21 +0000

    Day 11

--- 2024/aoc2024/day11/Day11.kt
+++ 2024/aoc2024/day11/Day11.kt
@@ -1,7 +1,6 @@
 package aoc2024.day11
 
 import common.*
-import java.math.*
 import java.util.HashMap.*
 import kotlin.math.*
 
@@ -10,8 +9,8 @@ private val puzzle = loadFiles("aoc2024/day11", "input.txt").single()
 
 internal fun main() {
     Day11.assertCorrect()
-    benchmark { part1(puzzle) } // 593µs
-    benchmark(100) { part2(puzzle) } // 21.5ms :(
+    benchmark { part1(puzzle) } // 442µs
+    benchmark(100) { part2(puzzle) } // 18.8ms :(
 }
 
 internal object Day11 : Challenge {
@@ -29,10 +28,9 @@ private fun part1(input: String): Long = input.splitToLongs(" ").sumOf { countVa
 
 private fun part2(input: String): Long = input.splitToLongs(" ").sumOf { countValuesBeneath(75, it) }
 
-fun countValuesBeneath(iterRemaining: Int, number: Long, cache: MutableMap<Pair<Long, Int>, Long> = newHashMap(120_000)): Long {
+fun countValuesBeneath(iterRemaining: Int, number: Long, cache: Array<MutableMap<Long, Long>> = Array(iterRemaining + 1) { newHashMap(2_000) }): Long {
     if (iterRemaining == 0) return 1
-    val cacheKey = number to iterRemaining
-    cache[cacheKey]?.let { return it }
+    cache[iterRemaining][number]?.let { return it }
 
     val digitCount = log10(number.toFloat()).toInt() + 1
     return if (number == 0L) {
@@ -44,7 +42,7 @@ fun countValuesBeneath(iterRemaining: Int, number: Long, cache: MutableMap<Pair<
         countValuesBeneath(iterRemaining - 1, left, cache) + countValuesBeneath(iterRemaining - 1, right, cache)
     } else {
         countValuesBeneath(iterRemaining - 1, number * 2024, cache)
-    }.also { cache[cacheKey] = it }
+    }.also { cache[iterRemaining][number] = it }
 }
 
 private val powersOfTen = longArrayOf(
