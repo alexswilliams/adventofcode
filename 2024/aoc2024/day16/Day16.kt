b7b7b9b9b3b8f96ee881d1911343e42b1b35bdab
Alex Williams <alex.williams1@starlingbank.com>
2024-12-16 17:27:41 +0000

    Day 16 Faster

--- 2024/aoc2024/day16/Day16.kt
+++ 2024/aoc2024/day16/Day16.kt
@@ -9,8 +9,8 @@ private val puzzle = loadFilesToGrids("aoc2024/day16", "input.txt").single()
 
 internal fun main() {
     Day16.assertCorrect()
-    benchmark(100) { part1(puzzle) } // 3.3ms
-    benchmark(1) { part2(puzzle) } // 1.8s :(
+    benchmark(100) { part1(puzzle) } // 4.3ms
+    benchmark(10) { part2(puzzle) } // 28.7ms
 }
 
 internal object Day16 : Challenge {
@@ -55,48 +55,49 @@ private fun part1(grid: Grid): Int = aStarLowestScore(grid)
 
 private fun part2(grid: Grid): Int = aStarAllShortestPaths(grid).flatten().distinct().size
 
+private fun pack(pos: Location1616, direction: Facing) = pos * 4 + direction.ordinal
+
 private fun aStarLowestScore(grid: Grid): Int {
     fillDeadEnds(grid)
     val start = grid.locationOf('S')
     val end = grid.locationOf('E')
 
-    data class Visited(val pos: Location1616, val direction: Facing)
     data class Work(val pos: Location1616, val direction: Facing, val path: PersistentList<Location1616>)
 
-    val visited = mutableMapOf(Visited(start, Facing.EAST) to 0)
+    val visited = mutableMapOf(pack(start, Facing.EAST) to 0)
     val work = TreeQueue<Work> { abs(it.pos.row() - end.row()) + abs(it.pos.col() - end.col()) }
     work.offer(Work(start, Facing.EAST, persistentListOf(start)), weight = 0)
 
     while (true) {
         val u = work.poll() ?: error("S to E not connected")
-        val scoreAtU = visited[Visited(u.pos, u.direction)] ?: error("Illegal State")
+        val scoreAtU = visited[pack(u.pos, u.direction)]!!
 
         val ahead = u.pos.advance(u.direction)
         if (ahead == end) return scoreAtU + 1
 
         if (grid.at(ahead) != '#') {
             val newScore = scoreAtU + 1
-            val oldScoreAtAhead = visited[Visited(ahead, u.direction)] ?: Int.MAX_VALUE
+            val oldScoreAtAhead = visited[pack(ahead, u.direction)] ?: Int.MAX_VALUE
             if (newScore < oldScoreAtAhead) {
-                visited[Visited(ahead, u.direction)] = newScore
+                visited[pack(ahead, u.direction)] = newScore
                 work.offerOrReposition(Work(ahead, u.direction, u.path.plus(ahead)), oldScoreAtAhead, newScore)
             }
         }
         val left = u.pos.advance(u.direction.turnLeft())
         if (grid.at(left) != '#') {
             val newScore = scoreAtU + 1000
-            val oldScoreAtUButTurnedLeft = visited[Visited(u.pos, u.direction.turnLeft())] ?: Int.MAX_VALUE
+            val oldScoreAtUButTurnedLeft = visited[pack(u.pos, u.direction.turnLeft())] ?: Int.MAX_VALUE
             if (newScore < oldScoreAtUButTurnedLeft) {
-                visited[Visited(u.pos, u.direction.turnLeft())] = newScore
+                visited[pack(u.pos, u.direction.turnLeft())] = newScore
                 work.offerOrReposition(Work(u.pos, u.direction.turnLeft(), u.path), oldScoreAtUButTurnedLeft, newScore)
             }
         }
         val right = u.pos.advance(u.direction.turnRight())
         if (grid.at(right) != '#') {
             val newScore = scoreAtU + 1000
-            val oldScoreAtUButTurnedRight = visited[Visited(u.pos, u.direction.turnRight())] ?: Int.MAX_VALUE
+            val oldScoreAtUButTurnedRight = visited[pack(u.pos, u.direction.turnRight())] ?: Int.MAX_VALUE
             if (newScore < oldScoreAtUButTurnedRight) {
-                visited[Visited(u.pos, u.direction.turnRight())] = newScore
+                visited[pack(u.pos, u.direction.turnRight())] = newScore
                 work.offerOrReposition(Work(u.pos, u.direction.turnRight(), u.path), oldScoreAtUButTurnedRight, newScore)
             }
         }
@@ -109,11 +110,10 @@ private fun aStarAllShortestPaths(grid: Grid): List<List<Location1616>> {
     val start = grid.locationOf('S')
     val end = grid.locationOf('E')
 
-    data class Visited(val pos: Location1616, val direction: Facing)
     data class Work(val pos: Location1616, val direction: Facing, val path: PersistentList<Location1616>, val score: Int)
 
-    val visited = mutableMapOf(Visited(start, Facing.EAST) to 0)
-    val work = TreeQueue<Work> { 0 }
+    val visited = mutableMapOf(pack(start, Facing.EAST) to 0)
+    val work = TreeQueue<Work> { abs(it.pos.row() - end.row()) + abs(it.pos.col() - end.col()) }
     work.offer(Work(start, Facing.EAST, persistentListOf(start), 0), weight = 0)
 
     val shortestPaths = mutableListOf<List<Location1616>>()
@@ -130,27 +130,27 @@ private fun aStarAllShortestPaths(grid: Grid): List<List<Location1616>> {
 
         if (grid.at(ahead) != '#') {
             val newScore = u.score + 1
-            val oldScore = visited[Visited(ahead, u.direction)] ?: Int.MAX_VALUE
-            if (newScore <= 75416 && ahead !in u.path && newScore <= oldScore) {
-                visited[Visited(ahead, u.direction)] = newScore
-                work.offerOrReposition(Work(ahead, u.direction, u.path.plus(ahead), newScore), oldScore, newScore)
+            val oldScore = visited[pack(ahead, u.direction)] ?: Int.MAX_VALUE
+            if (newScore <= bestScore && newScore <= oldScore) {
+                visited[pack(ahead, u.direction)] = newScore
+                work.offer(Work(ahead, u.direction, u.path.plus(ahead), newScore), newScore)
             }
         }
         val newScore = u.score + 1000
-        if (newScore > 75416) continue
+        if (newScore > bestScore) continue
         val left = u.pos.advance(u.direction.turnLeft())
         if (grid.at(left) != '#') {
-            val oldScore = visited[Visited(u.pos, u.direction.turnLeft())] ?: Int.MAX_VALUE
-            if (left !in u.path && newScore <= oldScore) {
-                visited[Visited(u.pos, u.direction.turnLeft())] = newScore
+            val oldScore = visited[pack(u.pos, u.direction.turnLeft())] ?: Int.MAX_VALUE
+            if (newScore <= oldScore) {
+                visited[pack(u.pos, u.direction.turnLeft())] = newScore
                 work.offer(Work(u.pos, u.direction.turnLeft(), u.path, newScore), newScore)
             }
         }
         val right = u.pos.advance(u.direction.turnRight())
         if (grid.at(right) != '#') {
-            val oldScore = visited[Visited(u.pos, u.direction.turnRight())] ?: Int.MAX_VALUE
-            if (right !in u.path && newScore <= oldScore) {
-                visited[Visited(u.pos, u.direction.turnRight())] = newScore
+            val oldScore = visited[pack(u.pos, u.direction.turnRight())] ?: Int.MAX_VALUE
+            if (newScore <= oldScore) {
+                visited[pack(u.pos, u.direction.turnRight())] = newScore
                 work.offer(Work(u.pos, u.direction.turnRight(), u.path, newScore), newScore)
             }
         }
