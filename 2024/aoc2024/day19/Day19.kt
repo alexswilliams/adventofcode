953fc25c2890bbf2e5ebba9d6ed69e6716a0eb39
Alex Williams <alex.williams1@starlingbank.com>
2024-12-19 19:42:20 +0000

    Day 18

--- 2024/aoc2024/day19/Day19.kt
+++ 2024/aoc2024/day19/Day19.kt
@@ -8,7 +8,7 @@ private val puzzle = loadFilesToLines("aoc2024/day19", "input.txt").single()
 internal fun main() {
     Day19.assertCorrect()
     benchmark(100) { part1(puzzle) } // 3.2ms
-    benchmark(100) { part2(puzzle) } // 10.4ms
+    benchmark(100) { part2(puzzle) } // 8.0ms
 }
 
 internal object Day19 : Challenge {
@@ -50,17 +50,17 @@ private fun isSolvable(pattern: String, sortedTowels: Array<String>, startAt: In
     return false.also { cache[startAt] = 0 }
 }
 
-private fun solutionCount(pattern: String, sortedTowels: Array<String>, cache: MutableMap<String, Long> = mutableMapOf()): Long {
-    cache[pattern]?.let { return it }
-    if (pattern.isEmpty()) return 1L
-    val indexOfFirstMatch = sortedTowels.binarySearch(pattern)
+private fun solutionCount(pattern: String, sortedTowels: Array<String>, startAt: Int = 0, cache: LongArray = LongArray(pattern.length) { -1 }): Long {
+    if (startAt == pattern.length) return 1L
+    cache[startAt].let { if (it >= 0) return it }
+    val indexOfFirstMatch = sortedTowels.binarySearch(pattern.substring(startAt))
     var nextMatch = if (indexOfFirstMatch < 0) -indexOfFirstMatch - 2 else indexOfFirstMatch
     var ways = 0L
-    while (nextMatch >= 0 && pattern[0] == sortedTowels[nextMatch][0]) {
-        if (pattern.startsWith(sortedTowels[nextMatch])) {
-            ways += solutionCount(pattern.substring(sortedTowels[nextMatch].length), sortedTowels, cache)
+    while (nextMatch >= 0 && pattern[startAt] == sortedTowels[nextMatch][0]) {
+        if (pattern.startsWith(sortedTowels[nextMatch], startAt)) {
+            ways += solutionCount(pattern, sortedTowels, startAt + sortedTowels[nextMatch].length, cache)
         }
         nextMatch--
     }
-    return ways.also { cache[pattern] = it }
+    return ways.also { cache[startAt] = it }
 }
