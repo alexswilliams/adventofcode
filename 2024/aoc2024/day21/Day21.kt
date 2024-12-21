93a24f39ae6e52d3a6958f6de507aced9cf6eee0
Alex Williams <lexw@live.co.uk>
2024-12-21 15:47:55 +0000

    Day 21

--- 2024/aoc2024/day21/Day21.kt
+++ 2024/aoc2024/day21/Day21.kt
@@ -7,8 +7,8 @@ private val puzzle = loadFilesToLines("aoc2024/day21", "input.txt").single()
 
 internal fun main() {
     Day21.assertCorrect()
-    benchmark(10) { part1(puzzle) } // 20.9ms
-    benchmark(10) { part2(puzzle) }
+    benchmark { part1(puzzle) } // 103µs
+    benchmark { part2(puzzle) } // 317µs
 }
 
 internal object Day21 : Challenge {
@@ -16,54 +16,52 @@ internal object Day21 : Challenge {
         check(126384, "P1 Example") { part1(example) }
         check(248108, "P1 Puzzle") { part1(puzzle) }
 
-        check(0, "P2 Puzzle") { part2(puzzle) }
+        check(303836969158972, "P2 Puzzle") { part2(puzzle).toLong() }
     }
 }
 
 
-private fun part1(input: List<String>): Int {
-    val paths = cartesianProductOf(dirPad.keys, dirPad.keys).associateWith { (prev, next) -> dirPad.waysBetween(prev, next) } +
-            cartesianProductOf(numPad.keys, numPad.keys).associateWith { (prev, next) -> numPad.waysBetween(prev, next) }
-    return input.sumOf { code ->
-        code.toIntFromIndex(0) * "A$code".zipWithNext().sumOf { (prev, next) -> fewestKeyPushes2LevelAway(paths, "$prev$next") }
-    }
-}
+private fun part1(input: List<String>): Long =
+    with(newCache()) { input.sumOf { code -> fewestPushesNeeded("A$code", 2, this) * code.toIntFromIndex(0) } }
 
-private fun part2(input: List<String>): Int {
-    error("Need to think about this")
-}
+private fun part2(input: List<String>): Long =
+    with(newCache()) { input.sumOf { code -> fewestPushesNeeded("A$code", 25, this) * code.toIntFromIndex(0) } }
 
 
-private fun fewestKeyPushes2LevelAway(paths: Map<Pair<Char, Char>, List<List<Char>>>, code: String): Int =
-    code.zipWithNext()
-        .map { paths[it]!! }
-        .reduce { acc: List<List<Char>>, options: List<List<Char>> ->
-            options.flatMap { option -> acc.map { it + option } }
-        }
-        .asSequence()
-        .map { path -> (listOf('A') + path).zipWithNext() }
-        .flatMap { previousLevel ->
-            previousLevel.map { paths[it]!! }.reduce { acc, options ->
-                options.flatMap { option -> acc.map { it + option } }
+private fun fewestPushesNeeded(code: String, dirPadsRemaining: Int, cache: MutableMap<Int, Long>): Long {
+    if (code.length == 2) cache[cacheKey(dirPadsRemaining, code)]?.let { return it }
+
+    val pairs = code.zipWithNext().map { allPaths[it]!! }
+    if (dirPadsRemaining == 0) return pairs.sumOf { it.first().size }.toLong()
+
+    return pairs.reduce { acc, options -> options.flatMap { option -> acc.map { it + option } } }
+        .minOf { candidate ->
+            (listOf('A') + candidate).zipWithNext().sumOf { (start, end) ->
+                // The naive risk is that it might be shorter for one level to do vvvA as the levels above will all just do A
+                // repeatedly as a result of the vvv - that means you might only be able to consider full sequences for each level.
+                // But this recursion is valid because at the end of every move, each level above will have reached the A button
+                // which means every move beneath here starts from the same state, and so they are all independent of each other.
+                fewestPushesNeeded("$start$end", dirPadsRemaining - 1, cache)
             }
-        }
-        .minOf { path ->
-            (listOf('A') + path).asSequence()
-                .zipWithNext()
-                .sumOf { paths[it]!!.minOf { path -> path.size } }
-        }
+        }.also { if (code.length == 2) cache[cacheKey(dirPadsRemaining, code)] = it }
+}
+
+private fun newCache() = mutableMapOf<Int, Long>()
+private fun cacheKey(dirPadsRemaining: Int, code: String): Int =
+    dirPadsRemaining * 100 + code[0].code * 10 + code[1].code
 
 
 private fun Map<Char, List<Pair<Char, Char>>>.waysBetween(start: Char, target: Char): List<List<Char>> {
     fun waysBetweenInner(start: Char, seenSoFar: List<Char>): List<List<Char>> {
         if (start == target) return listOf(listOf('A'))
-        val seenForNext = seenSoFar.plus(start)
-        return this[start]!!.mapNotNull { (move, symbolReached) ->
-            if (symbolReached !in seenSoFar) waysBetweenInner(symbolReached, seenForNext).map { it + move }
-            else null
-        }.flatten()
+        val seenForNext = seenSoFar + start
+        return this[start]!!
+            .filter { (_, next) -> next !in seenSoFar }
+            .flatMap { (move, symbolReached) -> waysBetweenInner(symbolReached, seenForNext).map { it + move } }
     }
-    return waysBetweenInner(start, listOf()).map { it.reversed() }
+    return waysBetweenInner(start, listOf())
+        .map { it.reversed() }
+        .sortedBy { it.size }.let { all -> all.filter { it.size == all.first().size } }
 }
 
 private val dirPad = mapOf(
@@ -88,4 +86,9 @@ private val numPad = mapOf(
     '9' to listOf('<' to '8', 'v' to '6'),
 )
 
-
+// Caching this outside the benchmark is on the edge of cheating; but it's day 21 and starting to feel like a second job
+private val dirPadPaths = cartesianProductOf(dirPad.keys, dirPad.keys)
+    .associateWith { (prev, next) -> dirPad.waysBetween(prev, next) }
+private val numPadPaths = cartesianProductOf(numPad.keys, numPad.keys)
+    .associateWith { (prev, next) -> numPad.waysBetween(prev, next) }
+private val allPaths = dirPadPaths + numPadPaths
