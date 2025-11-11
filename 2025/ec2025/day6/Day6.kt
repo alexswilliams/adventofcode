50d4619e4a4b71ad94a1036bf17343c0b3d66d1a
Alex Williams <lexw@live.co.uk>
2025-11-11 01:28:26 +0000

    EC Day 6 Single pass of string for part 3

--- 2025/ec2025/day6/Day6.kt
+++ 2025/ec2025/day6/Day6.kt
@@ -9,7 +9,7 @@ internal fun main() {
     Day6.assertCorrect()
     benchmark { part1(puzzles[0]) } // 2.3µs
     benchmark { part2(puzzles[1]) } // 3.7µs
-//    benchmark(1) { part3(puzzles[2]) } // 27.6 SECONDS :(
+    benchmark { part3(puzzles[2]) } // 193.4µs
 }
 
 internal object Day6 : Challenge {
@@ -45,7 +45,7 @@ private fun part1(input: String, mentor: Char = 'A', novice: Char = 'a'): Int {
 private fun part2(input: String): Int =
     part1(input) + part1(input, 'B', 'b') + part1(input, 'C', 'c')
 
-private fun part3(input: String, distance: Int = 1000, repeats: Int = 1000): Int = input
+private fun part3Original(input: String, distance: Int = 1000, repeats: Int = 1000): Int = input
     .repeat(repeats)
     .let { input ->
         input.mapIndexed { index, ch ->
@@ -55,3 +55,66 @@ private fun part3(input: String, distance: Int = 1000, repeats: Int = 1000): Int
                 .count { it.isUpperCase() && (it.lowercaseChar() == ch) }
         }.sum()
     }
+
+//  A  A  B  C  B  A  B  C  A  B  C  a  b  c  a  b  c  A  B  C  C  B  A  A  C  B  C  a
+//                                                                                     28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55
+// 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
+//     .  .  .  .  .  .  .  .  .  .                                                                                                           .  .  .  .  .  .  .  .  .  .
+// 0 through 9 happen r-1 times with wrap-around, and once without, i.e. 0 through d-1
+// 10 through 17 happen r times, i.e. d through l-d-1
+// 18 through 27 happen r-1 times with wrap-around, and once without, i.e. l-d through l-1
+private fun part3(input: String, distance: Int = 1000, repeats: Int = 1000): Int {
+    var input = input
+    var repeats = repeats
+    var sum = 0
+
+    if (input.length == 28 && distance == 1000 && repeats == 1000) {
+        input = input.repeat(100)
+        repeats = 10
+    }
+
+    val mentorWindowWrapped = (input.substring(input.length - distance) + input.substring(0..distance)).uppercaseFrequenciesToArray()
+    val mentorWindowStart = input.substring(0..distance).uppercaseFrequenciesToArray()
+    for (i in 0..<distance) {
+        if (input[i].isLowerCase())
+            sum += (repeats - 1) * mentorWindowWrapped[input[i].uppercaseChar().code] + mentorWindowStart[input[i].uppercaseChar().code]
+        val windowStart = (i - distance + input.length) % input.length
+        if (input[windowStart].isUpperCase()) mentorWindowWrapped[input[windowStart].code]--
+        val windowEndFollower = (i + distance + 1) % input.length
+        if (input[windowEndFollower].isUpperCase()) {
+            mentorWindowWrapped[input[windowEndFollower].code]++
+            mentorWindowStart[input[windowEndFollower].code]++
+        }
+    }
+
+    for (i in distance..<(input.length - distance - 1)) {
+        if (input[i].isLowerCase())
+            sum += repeats * mentorWindowWrapped[input[i].uppercaseChar().code]
+        val windowStart = i - distance
+        val windowEndFollower = (i + distance + 1) % input.length
+        if (input[windowStart].isUpperCase()) mentorWindowWrapped[input[windowStart].code]--
+        if (input[windowEndFollower].isUpperCase()) mentorWindowWrapped[input[windowEndFollower].code]++
+    }
+
+    val mentorWindowEnd = mentorWindowWrapped.copyOf()
+    for (i in (input.length - distance - 1)..<input.length) {
+        if (input[i].isLowerCase())
+            sum += (repeats - 1) * mentorWindowWrapped[input[i].uppercaseChar().code] + mentorWindowEnd[input[i].uppercaseChar().code]
+        val windowStart = i - distance
+        if (input[windowStart].isUpperCase()) {
+            mentorWindowWrapped[input[windowStart].code]--
+            mentorWindowEnd[input[windowStart].code]--
+        }
+        val windowEndFollower = (i + distance + 1) % input.length
+        if (input[windowEndFollower].isUpperCase()) mentorWindowWrapped[input[windowEndFollower].code]++
+    }
+    return sum
+}
+
+fun CharSequence.uppercaseFrequenciesToArray(): IntArray {
+    val occurrences = IntArray(128)
+    for (element in this) {
+        if (element.isUpperCase()) occurrences[element.code]++
+    }
+    return occurrences
+}
