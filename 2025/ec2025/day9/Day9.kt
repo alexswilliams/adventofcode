df5178ddd8c2b62f271da3ab8936e0bc38557778
Alex Williams <lexw@live.co.uk>
2025-11-14 02:28:37 +0000

    EC Day 9 Swap to XORing

--- 2025/ec2025/day9/Day9.kt
+++ 2025/ec2025/day9/Day9.kt
@@ -1,16 +1,19 @@
+@file:OptIn(ExperimentalUnsignedTypes::class)
+
 package ec2025.day9
 
 import common.*
 import java.lang.Integer.min
+import java.util.*
 
 private val examples = loadFilesToLines("ec2025/day9", "example1.txt", "example2.txt", "example3a.txt", "example3b.txt")
 private val puzzles = loadFilesToLines("ec2025/day9", "input1.txt", "input2.txt", "input3.txt")
 
 internal fun main() {
     Day9.assertCorrect()
-    benchmark { part1(puzzles[0]) } // 22.9µs
-    benchmark(100) { part2(puzzles[1]) } // 13.3ms
-    benchmark(1) { part3(puzzles[2]) } // 1.8s
+    benchmark { part1(puzzles[0]) } // 16.6µs
+    benchmark(100) { part2(puzzles[1]) } // 10.3ms
+    benchmark(1) { part3(puzzles[2]) } // 1.2s
 }
 
 internal object Day9 : Challenge {
@@ -29,58 +32,71 @@ internal object Day9 : Challenge {
 
 
 private fun part1(input: List<String>): Int =
-    input[0].substring(2).zip(input[2].substring(2)).count { it.second == it.first } *
-            input[1].substring(2).zip(input[2].substring(2)).count { it.second == it.first }
+    degreeOf(input.map { it.substringAfter(':') }, 0, 1, 2)
 
 private fun part2(input: List<String>): Int {
     val duckDna = input.map { it.substringAfter(':') }
-    var totalDegrees = 0
-    for (child in duckDna) {
-        val relations = triangularExclusiveSequenceOf(0, duckDna.lastIndex) { hi, lo -> duckDna[lo] to duckDna[hi] }
-            .filter { it.first != child && it.second != child }
-            .firstOrNull { (a, b) -> related(child, a, b) }
-        if (relations == null) continue
-        val degree = child.zip(relations.first).count { it.second == it.first } *
-                child.zip(relations.second).count { it.second == it.first }
-        totalDegrees += degree
+    val asLongs = duckDna.map { it.toDna() }
+    return duckDna.indices.sumOf { index ->
+        findParents(asLongs, index)?.let { (a, b) ->
+            degreeOf(duckDna, a, b, index)
+        } ?: 0
     }
-    return totalDegrees
 }
 
 private fun part3(input: List<String>): Int {
     val duckDna = input.map { it.substringAfter(':') }
+    val asLongs = duckDna.map { it.toDna() }
+
     val relationships = mutableMapOf<Int, MutableList<Int>>()
-    for ((index, child) in duckDna.withIndex()) {
-        val relations = triangularExclusiveSequenceOf(0, duckDna.lastIndex) { hi, lo -> lo to hi }
-            .filter { it.first != index && it.second != index }
-            .firstOrNull { (a, b) -> related(child, duckDna[a], duckDna[b]) }
-        if (relations == null) continue
-
-        relationships.getOrPut(min(index, relations.first) + 1) { mutableListOf() }.add(max(index, relations.first) + 1)
-        relationships.getOrPut(min(index, relations.second) + 1) { mutableListOf() }.add(max(index, relations.second) + 1)
+    for (index in duckDna.indices) {
+        val parents = findParents(asLongs, index) ?: continue
+        relationships.getOrPut(min(index, parents.first) + 1) { mutableListOf() }.add(max(index, parents.first) + 1)
+        relationships.getOrPut(min(index, parents.second) + 1) { mutableListOf() }.add(max(index, parents.second) + 1)
     }
 
-    val rels = relationships.entries.mapTo(ArrayList()) { (key, value) -> value.toMutableSet().apply { add(key) } }
+    val subsets = relationships.entries.mapTo(LinkedList()) { (key, value) -> value.toMutableSet().apply { add(key) } }
     while (true) {
-        val toMerge = triangularExclusiveSequenceOf(0, rels.lastIndex) { hi, lo -> lo to hi }
-            .firstOrNull { (lo, hi) -> (rels[lo] intersect rels[hi]).isNotEmpty() }
+        val toMerge = triangularExclusiveSequenceOf(0, subsets.lastIndex) { hi, lo -> lo to hi }
+            .firstOrNull { (lo, hi) -> (subsets[lo] intersect subsets[hi]).isNotEmpty() }
         if (toMerge == null) break
-        rels[toMerge.first].addAll(rels[toMerge.second])
-        rels.removeAt(toMerge.second)
+        subsets[toMerge.first].addAll(subsets[toMerge.second])
+        subsets.removeAt(toMerge.second)
+    }
+    return subsets.maxBy { it.size }.sum()
+}
+
+
+private fun degreeOf(duckDna: List<String>, parentA: Int, parentB: Int, child: Int): Int =
+    duckDna[child].zip(duckDna[parentA]).count { it.second == it.first } *
+            duckDna[child].zip(duckDna[parentB]).count { it.second == it.first }
+
+private fun findParents(duckDna: List<ULongArray>, childIndex: Int): Pair<Int, Int>? =
+    triangularExclusiveSequenceOf(0, duckDna.lastIndex) { hi, lo -> lo to hi }
+        .filter { it.first != childIndex && it.second != childIndex }
+        .firstOrNull { (a, b) -> related(duckDna[childIndex], duckDna[a], duckDna[b]) }
+
+
+private fun related(c: ULongArray, a: ULongArray, b: ULongArray): Boolean =
+    c.indices.all { i ->
+        val ai = a[i]
+        val bi = b[i]
+        val ci = c[i]
+        val diff = ai xor bi
+        val sames = ai and diff.inv() // parts where parents match
+        val aa = ci and diff and ai // parts where child and ONLY parent a match
+        val bb = ci and diff and bi // parts where child and ONLY parent b match
+        sames or aa or bb == ci // recombining all above parts should yield the child
     }
 
-    return rels.maxBy { it.size }.sum()
-
-    // thought...
-//    val a = 0b11011000u
-//    val b = 0b11010110u
-//    val c = 0b11011010u
-//    println((c and (a xor b).inv()) or (c and (a xor b) and a) or (c and (a xor b) and b) == c)
+private fun letterToQuad(letter: Char): ULong = when (letter) {
+    'C' -> 0b0001uL
+    'G' -> 0b0010uL
+    'A' -> 0b0100uL
+    'T' -> 0b1000uL
+    else -> throw Exception()
 }
 
-
-fun related(child: String, a: String, b: String): Boolean {
-    for (i in child.indices)
-        if (a[i] != child[i] && b[i] != child[i]) return false
-    return true
-}
+private fun String.toDna(): ULongArray =
+    this.windowed(16) { it.fold(0uL) { acc, ch -> (acc shl 4) or letterToQuad(ch) } }
+        .toULongArray()
